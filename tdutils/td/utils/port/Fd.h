6cf95c36addf120d77f461bff66ef76722517067
levlam <levlam@telegram.org>
2018-01-30 20:10:44 +0300

    Clear errno just in case.
    GitOrigin-RevId: b45ff66a66fba605e5a1b500a1ab66eb87140e19

--- tdutils/td/utils/port/Fd.h
+++ tdutils/td/utils/port/Fd.h
@@ -182,6 +182,7 @@ auto skip_eintr(F &&f) {
   decltype(f()) res;
   static_assert(std::is_integral<decltype(res)>::value, "integral type expected");
   do {
+    errno = 0;  // just in case
     res = f();
   } while (res < 0 && errno == EINTR);
   return res;
@@ -190,6 +191,7 @@ template <class F>
 auto skip_eintr_cstr(F &&f) {
   char *res;
   do {
+    errno = 0;  // just in case
     res = f();
   } while (res == nullptr && errno == EINTR);
   return res;
