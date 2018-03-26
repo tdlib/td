3c34e273292399d0ada8ae1240569e3013350d26
levlam <levlam@telegram.org>
2018-03-26 21:16:38 +0300

    Fix includes.
    GitOrigin-RevId: 9c64c8c5f15eef88247002251cc33f27749a1cd6

--- test/secure_storage.cpp
+++ test/secure_storage.cpp
@@ -4,9 +4,10 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-#include "td/utils/tests.h"
+#include "td/utils/buffer.h"
 #include "td/utils/filesystem.h"
 #include "td/utils/port/path.h"
+#include "td/utils/tests.h"
 
 #include "td/telegram/SecureStorage.h"
 
