717a307ff22c500d630ba0c6fe4263d75c2a8533
levlam <levlam@telegram.org>
2018-04-12 12:11:48 +0300

    Fix clang 3.4-3.5 CE (2).
    GitOrigin-RevId: 3ebf199d67ce42ea63add34aeb982debe8a9af3d

--- tdutils/td/utils/optional.h
+++ tdutils/td/utils/optional.h
@@ -16,7 +16,7 @@ template <class T>
 class optional {
  public:
   optional() = default;
-  template <class T1>
+  template <class T1, std::enable_if_t<!std::is_same<std::decay_t<T1>, optional>::value, int> = 0>
   optional(T1 &&t) : impl_(std::forward<T1>(t)) {
   }
   explicit operator bool() const {
