a532374f20d130b08de35959154e19c4474f3669
levlam <levlam@telegram.org>
2018-05-29 22:20:46 +0300

    Fix calls to register_remote.
    GitOrigin-RevId: a8d816f39065a30d840996b5b97b196377a38d25

--- td/telegram/SecureValue.cpp
+++ td/telegram/SecureValue.cpp
@@ -315,7 +315,7 @@ EncryptedSecureFile get_encrypted_secure_file(FileManager *file_manager,
       }
       result.file.file_id = file_manager->register_remote(
           FullRemoteFileLocation(FileType::Secure, secure_file->id_, secure_file->access_hash_, DcId::internal(dc_id)),
-          FileLocationSource::FromServer, {}, 0, 0, "");
+          FileLocationSource::FromServer, DialogId(), 0, secure_file->size_, to_string(secure_file->id_) + ".jpg");
       result.file.date = secure_file->date_;
       if (result.file.date < 0) {
         LOG(ERROR) << "Receive wrong date " << result.file.date;
@@ -379,7 +379,8 @@ static td_api::object_ptr<td_api::datedFile> get_dated_file_object(FileManager *
   dated_file.file_id = file_manager->register_remote(
       FullRemoteFileLocation(FileType::SecureRaw, file_view.remote_location().get_id(),
                              file_view.remote_location().get_access_hash(), file_view.remote_location().get_dc_id()),
-      FileLocationSource::FromServer, {}, 0, 0, "");
+      FileLocationSource::FromServer, DialogId(), file_view.size(), file_view.expected_size(),
+      file_view.suggested_name());
   return get_dated_file_object(file_manager, dated_file);
 }
 
