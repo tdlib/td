14f18504900259c74e5e84cd15089bbe245727e9
levlam <levlam@telegram.org>
2018-06-28 21:11:52 +0300

    Add new updates to Java example.
    GitOrigin-RevId: 6c13f8de7e4d3028b9660973c1e3cb78994c0ae2

--- example/java/org/drinkless/tdlib/example/Example.java
+++ example/java/org/drinkless/tdlib/example/Example.java
@@ -493,6 +493,31 @@ public final class Example {
                     }
                     break;
                 }
+                case TdApi.UpdateChatDefaultDisableNotification.CONSTRUCTOR: {
+                    TdApi.UpdateChatDefaultDisableNotification update = (TdApi.UpdateChatDefaultDisableNotification) object;
+                    TdApi.Chat chat = chats.get(update.chatId);
+                    synchronized (chat) {
+                        chat.defaultDisableNotification = update.defaultDisableNotification;
+                    }
+                    break;
+                }
+                case TdApi.UpdateChatIsMarkedAsUnread.CONSTRUCTOR: {
+                    TdApi.UpdateChatIsMarkedAsUnread update = (TdApi.UpdateChatIsMarkedAsUnread) object;
+                    TdApi.Chat chat = chats.get(update.chatId);
+                    synchronized (chat) {
+                        chat.isMarkedAsUnread = update.isMarkedAsUnread;
+                    }
+                    break;
+                }
+                case TdApi.UpdateChatIsSponsored.CONSTRUCTOR: {
+                    TdApi.UpdateChatIsSponsored updateChat = (TdApi.UpdateChatIsSponsored) object;
+                    TdApi.Chat chat = chats.get(updateChat.chatId);
+                    synchronized (chat) {
+                        chat.isSponsored = updateChat.isSponsored;
+                        setChatOrder(chat, updateChat.order);
+                    }
+                    break;
+                }
 
                 case TdApi.UpdateUserFullInfo.CONSTRUCTOR:
                     TdApi.UpdateUserFullInfo updateUserFullInfo = (TdApi.UpdateUserFullInfo) object;
