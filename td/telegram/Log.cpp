42fdbb5c0d0cb00498fb16b4ffea62e3e3db38d8
Mattia Basaglia <mattia.basaglia@tangent.co.uk>
2018-07-19 11:58:42 +0100

    Add a function to get the current log verbosity

--- td/telegram/Log.cpp
+++ td/telegram/Log.cpp
@@ -46,6 +46,10 @@ void Log::set_verbosity_level(int new_verbosity_level) {
   SET_VERBOSITY_LEVEL(VERBOSITY_NAME(FATAL) + new_verbosity_level);
 }
 
+int Log::get_verbosity_level() {
+  return GET_VERBOSITY_LEVEL();
+}
+
 void Log::set_fatal_error_callback(FatalErrorCallbackPtr callback) {
   if (callback == nullptr) {
     fatal_error_callback = nullptr;
