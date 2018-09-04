d9a18a064fa99130dc9214fb6131ba59e5660892
levlam <levlam@telegram.org>
2018-09-04 21:26:44 +0300

    Update version to 1.3.0.
    GitOrigin-RevId: 769ebff9a53dcf5087ecf691eb1c52dac9a85f7a

--- README.md
+++ README.md
@@ -121,7 +121,7 @@ target_link_libraries(YourTarget PRIVATE Td::TdStatic)
 
 Or you could install `TDLib` and then reference it in your CMakeLists.txt like this:
 ```
-find_package(Td 1.2.5 REQUIRED)
+find_package(Td 1.3.0 REQUIRED)
 target_link_libraries(YourTarget PRIVATE Td::TdStatic)
 ```
 See [example/cpp/CMakeLists.txt](https://github.com/tdlib/td/tree/master/example/cpp/CMakeLists.txt).
--- td/telegram/Td.h
+++ td/telegram/Td.h
@@ -203,7 +203,7 @@ class Td final : public NetQueryCallback {
   static td_api::object_ptr<td_api::Object> static_request(td_api::object_ptr<td_api::Function> function);
 
  private:
-  static constexpr const char *TDLIB_VERSION = "1.2.5";
+  static constexpr const char *TDLIB_VERSION = "1.3.0";
   static constexpr int64 ONLINE_ALARM_ID = 0;
   static constexpr int32 ONLINE_TIMEOUT = 240;
   static constexpr int64 PING_SERVER_ALARM_ID = -1;
