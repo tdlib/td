3347a5e2da284dfd317241c11723635672d4031f
levlam <levlam@telegram.org>
2018-12-02 22:26:27 +0300

    Fix OpenSSL armv7 build in iOS example.
    GitOrigin-RevId: 078d669b11f1aafc2d6ea26088d663e2bd4b06a0

--- example/ios/build-openssl.sh
+++ example/ios/build-openssl.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/pybee/Python-Apple-support
 cd Python-Apple-support
-git checkout 2.7
+git checkout 60b990128d5f1f04c336ff66594574515ab56604
 cd ..
 
 #TODO: change openssl version
