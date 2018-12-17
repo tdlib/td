b5e95821adfbd68fecc412348e1ab0b55badbb48
levlam <levlam@telegram.org>
2018-12-18 02:02:28 +0300

    Remove unsafe Slice operators.
    GitOrigin-RevId: 38555a755fd4252ccc25398340b7fcab542d9f3c

--- tdutils/td/utils/Slice-decl.h
+++ tdutils/td/utils/Slice-decl.h
@@ -85,6 +85,18 @@ class Slice {
   constexpr Slice(const char (&a)[N]) : s_(a), len_(N - 1) {
   }
 
+  Slice &operator=(string &&s) = delete;
+
+  template <size_t N>
+  constexpr Slice &operator=(char (&a)[N]) = delete;
+
+  template <size_t N>
+  constexpr Slice &operator=(const char (&a)[N]) {
+    s_ = a;
+    len_ = N - 1;
+    return *this;
+  }
+
   bool empty() const;
   size_t size() const;
 
@@ -168,6 +180,18 @@ class CSlice : public Slice {
   }
 
   CSlice() : CSlice("") {
+  }
+
+  CSlice &operator=(string &&s) = delete;
+
+  template <size_t N>
+  constexpr CSlice &operator=(char (&a)[N]) = delete;
+
+  template <size_t N>
+  constexpr CSlice &operator=(const char (&a)[N]) {
+    s_ = a;
+    len_ = N - 1;
+    return *this;
   }
 
   const char *c_str() const {
