198f0997c1be8a99a213c9a4efd9e323e79aa90c
levlam <levlam@telegram.org>
2018-12-28 20:18:39 +0300

    Simplify srs.{sh,ps1} scripts.
    GitOrigin-RevId: 6e80ac1d8e04a5c22a6a71fdda75fb23b2695753

--- src.ps1
+++ src.ps1
@@ -1,1 +1,1 @@
-﻿git ls-tree -r HEAD --name-only benchmark example memprof td tdactor tddb tdnet tdtl tdutils test tg_http_client | Select-String "\.cpp$|\.h$|\.hpp$" | Select-String -NotMatch "third_party"
+﻿git ls-tree -r HEAD --name-only benchmark example memprof td tdactor tddb tdnet tdtl tdutils test tg_http_client | Select-String "\.cpp$|\.h$|\.hpp$"
--- src.sh
+++ src.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git ls-tree -r HEAD --name-only benchmark example memprof td tdactor tddb tdnet tdtl tdutils test tg_http_client | grep -E "\.cpp$|\.h$|\.hpp$" | grep -v third_party
+git ls-tree -r HEAD --name-only benchmark example memprof td tdactor tddb tdnet tdtl tdutils test tg_http_client | grep -E "\.cpp$|\.h$|\.hpp$"
