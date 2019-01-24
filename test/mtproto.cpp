2a0db51cd900411d143cfabc7236784c0af3571f
Arseny Smirnov <arseny30@gmail.com>
2019-01-24 23:03:30 +0400

    GetHostByName: better test
    GitOrigin-RevId: 2be0f5e7b3122c7fd9122fcab763dffcad3d47ab

--- test/mtproto.cpp
+++ test/mtproto.cpp
@@ -58,7 +58,7 @@ TEST(Mtproto, GetHostByName) {
       GetHostByNameActor::resolve(host, options, std::move(promise)).release();
     };
 
-    std::vector<std::string> hosts = {"127.0.0.2", "1.1.1.1", "localhost", "web.telegram.org"};
+    std::vector<std::string> hosts = {"127.0.0.2", "1.1.1.1", "localhost", "web.telegram.org", "москва.рф"};
     for (auto host : hosts) {
       for (auto type : {GetHostByNameActor::ResolveType::Native, GetHostByNameActor::ResolveType::Google,
                         GetHostByNameActor::ResolveType::All}) {
