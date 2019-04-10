589edf60b1f4db9f8ac7b9ce4d1308025042e426
levlam <levlam@telegram.org>
2019-04-11 01:01:48 +0300

    Really fix documentation.
    GitOrigin-RevId: 0114466a4d0b97c8f8ab336fb30f780eac4010c5

--- td/generate/TlDocumentationGenerator.php
+++ td/generate/TlDocumentationGenerator.php
@@ -242,7 +242,7 @@ abstract class TlDocumentationGenerator
                 $this->addClassDocumentation($class_name, $base_class_name, $class_description);
 
                 foreach ($known_fields as $name => $field_type) {
-                    $may_be_null = stripos($info[$field_name], 'may be null') !== false;
+                    $may_be_null = stripos($info[$name], 'may be null') !== false;
                     $field_name = $this->getFieldName($name, $class_name);
                     $field_type_name = $this->getTypeName($field_type);
                     $this->addFieldDocumentation($class_name, $field_name, $field_type_name, $info[$name], $may_be_null);
