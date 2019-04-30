91a9bfa1d3dbaad36a9437bb62db1618e61bea00
levlam <levlam@telegram.org>
2019-04-30 03:21:06 +0300

    tdweb: fix warnings.
    GitOrigin-RevId: a94ecdb9c7b851e0d1842b07b5e5396e70c53387

--- example/web/tdweb/src/index.js
+++ example/web/tdweb/src/index.js
@@ -1,5 +1,5 @@
 import MyWorker from './worker.js';
-import localforage from 'localforage';
+//import localforage from 'localforage';
 //import './third_party/broadcastchannel.js';
 import BroadcastChannel from 'broadcast-channel';
 import uuid4 from 'uuid/v4';
@@ -129,7 +129,7 @@ class TdClient {
       });
       return;
     }
-    if (query['@type'] === 'readFile' || query['@type'] == 'readFilePart') {
+    if (query['@type'] === 'readFile' || query['@type'] === 'readFilePart') {
       this.readFile(query);
       return;
     }
