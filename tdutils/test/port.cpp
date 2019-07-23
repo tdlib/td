a4b67b949652f5a0dc676742abab088380c59210
levlam <levlam@telegram.org>
2019-07-23 03:53:46 +0300

    Change check for element uniqueness.
    GitOrigin-RevId: 3a7499f3b7f1aead7ebe80f147b9ddc42c87fc33

--- tdutils/test/port.cpp
+++ tdutils/test/port.cpp
@@ -129,7 +129,6 @@ TEST(Port, Writev) {
 
 #include <algorithm>
 #include <mutex>
-#include <set>
 
 static std::mutex m;
 static std::vector<std::string> ptrs;
@@ -197,9 +196,9 @@ TEST(Post, SignalsAndThread) {
     }
     std::sort(ptrs.begin(), ptrs.end());
     CHECK(ptrs == ans);
-    ASSERT_EQ(10u, std::set<int *>(addrs.begin(), addrs.end()).size());
+    std::sort(addrs.begin(), addrs.end());
+    ASSERT_TRUE(std::unique(addrs.begin(), addrs.end()) == addrs.end());
     //LOG(ERROR) << addrs;
   }
-  //ASSERT_EQ(10u, ptrs.size());
 }
 #endif
