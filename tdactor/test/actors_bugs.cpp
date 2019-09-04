e6597eca8cd24b917a1e48fc80ff47923c979309
levlam <levlam@telegram.org>
2019-09-04 22:38:51 +0300

    Add MultiTimeout test.
    GitOrigin-RevId: b8f381ff69971a76110bd7e9d94107012942f02e

--- tdactor/test/actors_bugs.cpp
+++ tdactor/test/actors_bugs.cpp
@@ -4,6 +4,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#include "td/utils/Random.h"
 #include "td/utils/tests.h"
 
 #include "td/actor/actor.h"
@@ -43,6 +44,71 @@ TEST(MultiTimeout, bug) {
 
   while (sched.run_main(10)) {
     // empty
+  }
+  sched.finish();
+}
+
+class TimeoutManager : public Actor {
+ public:
+  static int32 count;
+
+  TimeoutManager() {
+    count++;
+
+    test_timeout_.set_callback(on_test_timeout_callback);
+    test_timeout_.set_callback_data(static_cast<void *>(this));
+  }
+  TimeoutManager(const TimeoutManager &) = delete;
+  TimeoutManager &operator=(const TimeoutManager &) = delete;
+  TimeoutManager(TimeoutManager &&) = delete;
+  TimeoutManager &operator=(TimeoutManager &&) = delete;
+  ~TimeoutManager() override {
+    count--;
+    LOG(INFO) << "Destroy TimeoutManager";
+  }
+
+  static void on_test_timeout_callback(void *timeout_manager_ptr, int64 id) {
+    CHECK(count >= 0);
+    if (count == 0) {
+      LOG(ERROR) << "Receive timeout after manager was closed";
+      return;
+    }
+
+    auto manager = static_cast<TimeoutManager *>(timeout_manager_ptr);
+    send_closure_later(manager->actor_id(manager), &TimeoutManager::test_timeout);
+  }
+
+  void test_timeout() {
+    CHECK(count > 0);
+  }
+
+  MultiTimeout test_timeout_{"TestTimeout"};
+};
+
+int32 TimeoutManager::count;
+
+TEST(MultiTimeout, Destroy) {
+  SET_VERBOSITY_LEVEL(VERBOSITY_NAME(ERROR));
+  ConcurrentScheduler sched;
+  int threads_n = 0;
+  sched.init(threads_n);
+
+  ActorOwn<TimeoutManager> timeout_manager = sched.create_actor_unsafe<TimeoutManager>(0, "TimeoutManager");
+  TimeoutManager *manager = timeout_manager.get().get_actor_unsafe();
+  sched.start();
+  int cnt = 100;
+  while (sched.run_main(cnt == 100 || cnt <= 0 ? 0.001 : 10)) {
+    auto guard = sched.get_main_guard();
+    cnt--;
+    if (cnt > 0) {
+      for (int i = 0; i < 2; i++) {
+        manager->test_timeout_.set_timeout_in(Random::fast(0, 1000000000), Random::fast(2, 5) / 1000.0);
+      }
+    } else if (cnt == 0) {
+      timeout_manager.reset();
+    } else if (cnt == -10) {
+      Scheduler::instance()->finish();
+    }
   }
   sched.finish();
 }
