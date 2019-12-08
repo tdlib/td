c0b8349a56f2939771cd391d8065c133fa5f66dc
levlam <levlam@telegram.org>
2019-12-08 08:18:49 +0300

    Simplify test.
    GitOrigin-RevId: 033c85175c4e62059b377ace0e824e410f27b85e

--- tdutils/test/filesystem.cpp
+++ tdutils/test/filesystem.cpp
@@ -5,38 +5,43 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #include "td/utils/filesystem.h"
+#include "td/utils/Slice.h"
 #include "td/utils/tests.h"
 
+static void test_clean_filename(td::CSlice name, td::Slice result) {
+  ASSERT_STREQ(td::clean_filename(name), result);
+}
+
 TEST(Misc, clean_filename) {
-  using td::clean_filename;
-  ASSERT_STREQ(clean_filename("-1234567"), "-1234567");
-  ASSERT_STREQ(clean_filename(".git"), "git");
-  ASSERT_STREQ(clean_filename("../../.git"), "git");
-  ASSERT_STREQ(clean_filename(".././.."), "");
-  ASSERT_STREQ(clean_filename("../"), "");
-  ASSERT_STREQ(clean_filename(".."), "");
-  ASSERT_STREQ(clean_filename("test/git/   as   dsa  .   a"), "as   dsa.a");
-  ASSERT_STREQ(clean_filename("     .    "), "");
-  ASSERT_STREQ(clean_filename("!@#$%^&*()_+-=[]{;|:\"}'<>?,.`~"), "!@#$%^  ()_+-=[]{;   }    ,.~");
-  ASSERT_STREQ(clean_filename("!@#$%^&*()_+-=[]{}\\|:\";'<>?,.`~"), ";    ,.~");
-  ASSERT_STREQ(clean_filename("عرفها بعد قد. هذا مع تاريخ اليميني واندونيسيا،, لعدم تاريخ لهيمنة الى"),
-               "عرفها بعد قد.هذا مع تاريخ اليميني");
-  ASSERT_STREQ(
-      clean_filename(
-          "012345678901234567890123456789012345678901234567890123456789adsasdasdsaa.01234567890123456789asdasdasdasd"),
+  test_clean_filename("-1234567", "-1234567");
+  test_clean_filename(".git", "git");
+  test_clean_filename("../../.git", "git");
+  test_clean_filename(".././..", "");
+  test_clean_filename("../", "");
+  test_clean_filename("..", "");
+  test_clean_filename("test/git/   as   dsa  .   a", "as   dsa.a");
+  test_clean_filename("     .    ", "");
+  test_clean_filename("!@#$%^&*()_+-=[]{;|:\"}'<>?,.`~", "!@#$%^  ()_+-=[]{;   }    ,.~");
+  test_clean_filename("!@#$%^&*()_+-=[]{}\\|:\";'<>?,.`~", ";    ,.~");
+  test_clean_filename("عرفها بعد قد. هذا مع تاريخ اليميني واندونيسيا،, لعدم تاريخ لهيمنة الى",
+                      "عرفها بعد قد.هذا مع تاريخ اليميني");
+  test_clean_filename(
+      "012345678901234567890123456789012345678901234567890123456789adsasdasdsaa.01234567890123456789asdasdasdasd",
       "012345678901234567890123456789012345678901234567890123456789.01234567890123456789");
-  ASSERT_STREQ(clean_filename("01234567890123456789012345678901234567890123456789<>*?: <>*?:0123456789adsasdasdsaa.   "
-                              "0123456789`<><<>><><>0123456789asdasdasdasd"),
-               "01234567890123456789012345678901234567890123456789.0123456789");
-  ASSERT_STREQ(clean_filename("01234567890123456789012345678901234567890123456789<>*?: <>*?:0123456789adsasdasdsaa.   "
-                              "0123456789`<><><>0123456789asdasdasdasd"),
-               "01234567890123456789012345678901234567890123456789.0123456789       012");
-  ASSERT_STREQ(clean_filename("C:/document.tar.gz"), "document.tar.gz");
-  ASSERT_STREQ(clean_filename("test...."), "test");
-  ASSERT_STREQ(clean_filename("....test"), "test");
-  ASSERT_STREQ(clean_filename("test.exe...."), "test.exe");  // extension has changed
-  ASSERT_STREQ(clean_filename("test.exe01234567890123456789...."),
-               "test.exe01234567890123456789");  // extension may be more than 20 characters
-  ASSERT_STREQ(clean_filename("....test....asdf"), "test.asdf");
-  ASSERT_STREQ(clean_filename("കറുപ്പ്.txt"), "കറപപ.txt");
+  test_clean_filename(
+      "01234567890123456789012345678901234567890123456789<>*?: <>*?:0123456789adsasdasdsaa.   "
+      "0123456789`<><<>><><>0123456789asdasdasdasd",
+      "01234567890123456789012345678901234567890123456789.0123456789");
+  test_clean_filename(
+      "01234567890123456789012345678901234567890123456789<>*?: <>*?:0123456789adsasdasdsaa.   "
+      "0123456789`<><><>0123456789asdasdasdasd",
+      "01234567890123456789012345678901234567890123456789.0123456789       012");
+  test_clean_filename("C:/document.tar.gz", "document.tar.gz");
+  test_clean_filename("test....", "test");
+  test_clean_filename("....test", "test");
+  test_clean_filename("test.exe....", "test.exe");  // extension has changed
+  test_clean_filename("test.exe01234567890123456789....",
+                      "test.exe01234567890123456789");  // extension may be more than 20 characters
+  test_clean_filename("....test....asdf", "test.asdf");
+  test_clean_filename("കറുപ്പ്.txt", "കറപപ.txt");
 }
