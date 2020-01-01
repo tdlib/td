80c35676a2eb1e9b71db355ee217bba79fbdce31
levlam <levlam@telegram.org>
2020-01-01 04:23:48 +0300

    Update copyright year.
    GitOrigin-RevId: 09afb551b6e637dc69739fa735b0051a38b9e14c

--- example/uwp/app/Properties/AssemblyInfo.cs
+++ example/uwp/app/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("App2")]
-[assembly: AssemblyCopyright("Copyright © 2015-2019")]
+[assembly: AssemblyCopyright("Copyright © 2015-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
