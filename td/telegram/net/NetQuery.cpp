c25a346f5477bcc3b91a1f71522b5ac3903d61e9
levlam <levlam@telegram.org>
2020-01-05 04:22:03 +0300

    Ignore BOT_METHOD_INVALID for getAppConfig.
    GitOrigin-RevId: 953c9c9c20b31cb7118233db82d146e4ee384a6f

--- td/telegram/net/NetQuery.cpp
+++ td/telegram/net/NetQuery.cpp
@@ -54,7 +54,8 @@ void NetQuery::set_error(Status status, string source) {
   }
   if (status.message() == "BOT_METHOD_INVALID") {
     auto id = tl_constructor();
-    if (id != telegram_api::help_getNearestDc::ID && id != telegram_api::help_getProxyData::ID) {
+    if (id != telegram_api::help_getNearestDc::ID && id != telegram_api::help_getProxyData::ID &&
+        id != telegram_api::help_getAppConfig::ID) {
       LOG(ERROR) << "Receive BOT_METHOD_INVALID for query " << format::as_hex(id);
     }
   }
