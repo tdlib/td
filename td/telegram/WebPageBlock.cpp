bb02dfacd69fa5a5734cd6cf8c676afade8e8376
levlam <levlam@telegram.org>
2020-03-03 01:33:06 +0300

    Add protection from references inside richTextReference.
    GitOrigin-RevId: 2ed62fd49af95559c3e8453ee03374106d08b7d5

--- td/telegram/WebPageBlock.cpp
+++ td/telegram/WebPageBlock.cpp
@@ -128,8 +128,11 @@ class RichText {
               if (it->second == nullptr) {
                 return make_tl_object<td_api::richTextAnchorLink>(texts[0].get_rich_text_object(context), anchor.str());
               } else {
+                context->is_first_pass_ = true;
+                auto reference_text = it->second->get_rich_text_object(context);
+                context->is_first_pass_ = false;
                 return make_tl_object<td_api::richTextReference>(texts[0].get_rich_text_object(context),
-                                                                 it->second->get_rich_text_object(context));
+                                                                 std::move(reference_text));
               }
             }
           }
