a726450e0b6c94fc2782f482fc39092a28450e5c
levlam <levlam@telegram.org>
2020-06-10 22:18:22 +0300

    Remove unneded template parameter.
    GitOrigin-RevId: 77428c1488b833d95d0ed792054a295a4c182e19

--- tddb/td/db/binlog/BinlogHelper.h
+++ tddb/td/db/binlog/BinlogHelper.h
@@ -11,18 +11,19 @@
 #include "td/db/binlog/BinlogEvent.h"
 
 #include "td/utils/common.h"
+#include "td/utils/StorerBase.h"
 
 namespace td {
 
-template <class BinlogT, class StorerT>
-uint64 binlog_add(const BinlogT &binlog_ptr, int32 type, const StorerT &storer, Promise<> promise = Promise<>()) {
+template <class BinlogT>
+uint64 binlog_add(const BinlogT &binlog_ptr, int32 type, const Storer &storer, Promise<> promise = Promise<>()) {
   auto logevent_id = binlog_ptr->next_id();
   binlog_ptr->add_raw_event(logevent_id, BinlogEvent::create_raw(logevent_id, type, 0, storer), std::move(promise));
   return logevent_id;
 }
 
-template <class BinlogT, class StorerT>
-uint64 binlog_rewrite(const BinlogT &binlog_ptr, uint64 logevent_id, int32 type, const StorerT &storer,
+template <class BinlogT>
+uint64 binlog_rewrite(const BinlogT &binlog_ptr, uint64 logevent_id, int32 type, const Storer &storer,
                       Promise<> promise = Promise<>()) {
   auto seq_no = binlog_ptr->next_id();
   binlog_ptr->add_raw_event(seq_no, BinlogEvent::create_raw(logevent_id, type, BinlogEvent::Flags::Rewrite, storer),
