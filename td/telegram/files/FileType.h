d75c6eff57520221241b8531805d263b9e2cd031
levlam <levlam@telegram.org>
2020-06-22 02:20:48 +0300

    Add FileType.cpp.
    GitOrigin-RevId: 8ed89c8f90d3d83d810bdaede43d839962fea6fe

--- td/telegram/files/FileType.h
+++ td/telegram/files/FileType.h
@@ -37,178 +37,20 @@ enum class FileType : int32 {
   None
 };
 
-inline FileType from_td_api(const td_api::FileType &file_type) {
-  switch (file_type.get_id()) {
-    case td_api::fileTypeThumbnail::ID:
-      return FileType::Thumbnail;
-    case td_api::fileTypeProfilePhoto::ID:
-      return FileType::ProfilePhoto;
-    case td_api::fileTypePhoto::ID:
-      return FileType::Photo;
-    case td_api::fileTypeVoiceNote::ID:
-      return FileType::VoiceNote;
-    case td_api::fileTypeVideo::ID:
-      return FileType::Video;
-    case td_api::fileTypeDocument::ID:
-      return FileType::Document;
-    case td_api::fileTypeSecret::ID:
-      return FileType::Encrypted;
-    case td_api::fileTypeUnknown::ID:
-      return FileType::Temp;
-    case td_api::fileTypeSticker::ID:
-      return FileType::Sticker;
-    case td_api::fileTypeAudio::ID:
-      return FileType::Audio;
-    case td_api::fileTypeAnimation::ID:
-      return FileType::Animation;
-    case td_api::fileTypeSecretThumbnail::ID:
-      return FileType::EncryptedThumbnail;
-    case td_api::fileTypeWallpaper::ID:
-      return FileType::Background;
-    case td_api::fileTypeVideoNote::ID:
-      return FileType::VideoNote;
-    case td_api::fileTypeSecure::ID:
-      return FileType::Secure;
-    case td_api::fileTypeNone::ID:
-      return FileType::None;
-    default:
-      UNREACHABLE();
-      return FileType::None;
-  }
-}
-
-inline tl_object_ptr<td_api::FileType> as_td_api(FileType file_type) {
-  switch (file_type) {
-    case FileType::Thumbnail:
-      return make_tl_object<td_api::fileTypeThumbnail>();
-    case FileType::ProfilePhoto:
-      return make_tl_object<td_api::fileTypeProfilePhoto>();
-    case FileType::Photo:
-      return make_tl_object<td_api::fileTypePhoto>();
-    case FileType::VoiceNote:
-      return make_tl_object<td_api::fileTypeVoiceNote>();
-    case FileType::Video:
-      return make_tl_object<td_api::fileTypeVideo>();
-    case FileType::Document:
-      return make_tl_object<td_api::fileTypeDocument>();
-    case FileType::Encrypted:
-      return make_tl_object<td_api::fileTypeSecret>();
-    case FileType::Temp:
-      return make_tl_object<td_api::fileTypeUnknown>();
-    case FileType::Sticker:
-      return make_tl_object<td_api::fileTypeSticker>();
-    case FileType::Audio:
-      return make_tl_object<td_api::fileTypeAudio>();
-    case FileType::Animation:
-      return make_tl_object<td_api::fileTypeAnimation>();
-    case FileType::EncryptedThumbnail:
-      return make_tl_object<td_api::fileTypeSecretThumbnail>();
-    case FileType::Wallpaper:
-      return make_tl_object<td_api::fileTypeWallpaper>();
-    case FileType::VideoNote:
-      return make_tl_object<td_api::fileTypeVideoNote>();
-    case FileType::Secure:
-      return make_tl_object<td_api::fileTypeSecure>();
-    case FileType::SecureRaw:
-      UNREACHABLE();
-      return make_tl_object<td_api::fileTypeSecure>();
-    case FileType::Background:
-      return make_tl_object<td_api::fileTypeWallpaper>();
-    case FileType::DocumentAsFile:
-      return make_tl_object<td_api::fileTypeDocument>();
-    case FileType::None:
-      return make_tl_object<td_api::fileTypeNone>();
-    default:
-      UNREACHABLE();
-      return nullptr;
-  }
-}
+enum class FileDirType : int8 { Secure, Common };
 
 constexpr int32 file_type_size = static_cast<int32>(FileType::Size);
 
-inline CSlice get_file_type_name(FileType file_type) {
-  switch (file_type) {
-    case FileType::Thumbnail:
-      return CSlice("thumbnails");
-    case FileType::ProfilePhoto:
-      return CSlice("profile_photos");
-    case FileType::Photo:
-      return CSlice("photos");
-    case FileType::VoiceNote:
-      return CSlice("voice");
-    case FileType::Video:
-      return CSlice("videos");
-    case FileType::Document:
-      return CSlice("documents");
-    case FileType::Encrypted:
-      return CSlice("secret");
-    case FileType::Temp:
-      return CSlice("temp");
-    case FileType::Sticker:
-      return CSlice("stickers");
-    case FileType::Audio:
-      return CSlice("music");
-    case FileType::Animation:
-      return CSlice("animations");
-    case FileType::EncryptedThumbnail:
-      return CSlice("secret_thumbnails");
-    case FileType::Wallpaper:
-      return CSlice("wallpapers");
-    case FileType::VideoNote:
-      return CSlice("video_notes");
-    case FileType::SecureRaw:
-      return CSlice("passport");
-    case FileType::Secure:
-      return CSlice("passport");
-    case FileType::Background:
-      return CSlice("wallpapers");
-    case FileType::DocumentAsFile:
-      return CSlice("documents");
-    case FileType::Size:
-    case FileType::None:
-    default:
-      UNREACHABLE();
-      return CSlice("none");
-  }
-}
+FileType from_td_api(const td_api::FileType &file_type);
 
-inline StringBuilder &operator<<(StringBuilder &string_builder, FileType file_type) {
-  return string_builder << get_file_type_name(file_type);
-}
+tl_object_ptr<td_api::FileType> as_td_api(FileType file_type);
 
-enum class FileDirType : int8 { Secure, Common };
+CSlice get_file_type_name(FileType file_type);
 
-inline FileDirType get_file_dir_type(FileType file_type) {
-  switch (file_type) {
-    case FileType::Thumbnail:
-    case FileType::ProfilePhoto:
-    case FileType::Encrypted:
-    case FileType::Sticker:
-    case FileType::Temp:
-    case FileType::Wallpaper:
-    case FileType::EncryptedThumbnail:
-    case FileType::Secure:
-    case FileType::SecureRaw:
-    case FileType::Background:
-      return FileDirType::Secure;
-    default:
-      return FileDirType::Common;
-  }
-}
+StringBuilder &operator<<(StringBuilder &string_builder, FileType file_type);
 
-inline bool is_file_big(FileType file_type, int64 expected_size) {
-  switch (file_type) {
-    case FileType::Thumbnail:
-    case FileType::ProfilePhoto:
-    case FileType::Photo:
-    case FileType::EncryptedThumbnail:
-      return false;
-    default:
-      break;
-  }
+FileDirType get_file_dir_type(FileType file_type);
 
-  constexpr int64 SMALL_FILE_MAX_SIZE = 10 << 20;
-  return expected_size > SMALL_FILE_MAX_SIZE;
-}
+bool is_file_big(FileType file_type, int64 expected_size);
 
 }  // namespace td
