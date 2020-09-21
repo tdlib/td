8503e7f214db30b5fdaea3cad4fd140b33f27d1f
levlam <levlam@telegram.org>
2020-09-22 01:41:46 +0300

    Update SplitSource.php.
    GitOrigin-RevId: 9e4dfaa6d15e82f3f69ce7149dfb5e0c9df99f76

--- SplitSource.php
+++ SplitSource.php
@@ -273,7 +273,7 @@ function split_file($file, $chunks, $undo) {
                 'HashtagHints' => 'HashtagHints',
                 'inline_queries_manager[_(-][^.]|InlineQueriesManager' => 'InlineQueriesManager',
                 'language_pack_manager[_(-][^.]|LanguagePackManager' => 'LanguagePackManager',
-                'get_erase_logevent_promise|parse_time|store_time' => 'logevent/LogEventHelper',
+                'LogeventIdWithGeneration|add_log_event|delete_log_event|get_erase_logevent_promise|parse_time|store_time' => 'logevent/LogEventHelper',
                 'MessageCopyOptions' => 'MessageCopyOptions',
                 'messages_manager[_(-][^.]|MessagesManager' => 'MessagesManager',
                 'notification_manager[_(-][^.]|NotificationManager|notifications[)]' => 'NotificationManager',
