1aff1e75efd7472339137ef2892b2a1156c14396
levlam <levlam@telegram.org>
2020-10-13 02:30:40 +0300

    Fix net query Counter initialization.
    GitOrigin-RevId: ba23b816cfed060cea15244e3f2264f74c89d390

--- td/telegram/net/NetQueryStats.h
+++ td/telegram/net/NetQueryStats.h
@@ -41,7 +41,7 @@ class NetQueryStats {
   void dump_pending_network_queries();
 
  private:
-  NetQueryCounter::Counter count_;
+  NetQueryCounter::Counter count_{0};
   std::atomic<bool> use_list_{true};
   TsList<NetQueryDebug> list_;
 };
