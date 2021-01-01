52c1da0a7026f575622f2c51c9d83feb153dc10c
levlam <levlam@telegram.org>
2021-01-01 15:57:46 +0300

    Update copyright year.

--- example/uwp/app/Properties/AssemblyInfo.cs
+++ example/uwp/app/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("App2")]
-[assembly: AssemblyCopyright("Copyright © 2015-2020")]
+[assembly: AssemblyCopyright("Copyright © 2015-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
