5d0561ff7a725f8e48042df35bbd72714a34ce6e
levlam <levlam@telegram.org>
2021-04-06 01:00:31 +0300

    Improve payment receipt.

--- td/telegram/Payments.cpp
+++ td/telegram/Payments.cpp
@@ -457,14 +457,22 @@ class GetPaymentReceiptQuery : public Td::ResultHandler {
     UserId payments_provider_user_id(payment_receipt->provider_id_);
     if (!payments_provider_user_id.is_valid()) {
       LOG(ERROR) << "Receive invalid payments provider " << payments_provider_user_id;
-      payments_provider_user_id = UserId();
+      return on_error(id, Status::Error(500, "Receive invalid payments provider identifier"));
     }
+    UserId seller_bot_user_id(payment_receipt->bot_id_);
+    if (!seller_bot_user_id.is_valid()) {
+      LOG(ERROR) << "Receive invalid seller " << seller_bot_user_id;
+      return on_error(id, Status::Error(500, "Receive invalid seller identifier"));
+    }
+    auto photo = get_web_document_photo(td->file_manager_.get(), std::move(payment_receipt->photo_), dialog_id_);
 
     promise_.set_value(make_tl_object<td_api::paymentReceipt>(
-        payment_receipt->date_, td->contacts_manager_->get_user_id_object(payments_provider_user_id, "paymentReceipt"),
+        payment_receipt->title_, payment_receipt->description_, get_photo_object(td->file_manager_.get(), photo),
+        payment_receipt->date_, td->contacts_manager_->get_user_id_object(seller_bot_user_id, "paymentReceipt seller"),
+        td->contacts_manager_->get_user_id_object(payments_provider_user_id, "paymentReceipt provider"),
         convert_invoice(std::move(payment_receipt->invoice_)), convert_order_info(std::move(payment_receipt->info_)),
-        convert_shipping_option(std::move(payment_receipt->shipping_)),
-        std::move(payment_receipt->credentials_title_)));
+        convert_shipping_option(std::move(payment_receipt->shipping_)), std::move(payment_receipt->credentials_title_),
+        payment_receipt->tip_amount_));
   }
 
   void on_error(uint64 id, Status status) override {
