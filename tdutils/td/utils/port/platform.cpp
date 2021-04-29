e1fc85d6101988f1a79102936923c8841fad9828
levlam <levlam@telegram.org>
2021-04-30 01:24:42 +0300

    Print warnings about unsupported OS/compiler only once.

---
+++ tdutils/td/utils/port/platform.cpp
@@ -0,0 +1,23 @@
+//
+// Copyright Aliaksei Levin (levlam@telegram.org), Arseny Smirnov (arseny30@gmail.com) 2014-2021
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "td/utils/port/platform.h"
+
+#if !TD_MSVC
+
+#if TD_DARWIN_UNKNOWN
+#warning "Probably unsupported Apple operating system. Feel free to try to compile"
+#endif
+
+#if TD_UNIX_UNKNOWN
+#warning "Probably unsupported Unix operating system. Feel free to try to compile"
+#endif
+
+#if TD_COMPILER_UNKNOWN
+#warning "Probably unsupported compiler. Feel free to try to compile"
+#endif
+
+#endif
