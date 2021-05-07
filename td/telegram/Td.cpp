9c49e6a62553f60ffae878c06de170237477259a
levlam <levlam@telegram.org>
2021-05-07 20:52:54 +0300

    Log versions on Td instance creation.

--- td/telegram/Td.cpp
+++ td/telegram/Td.cpp
@@ -95,6 +95,7 @@
 #include "td/telegram/TopDialogCategory.h"
 #include "td/telegram/TopDialogManager.h"
 #include "td/telegram/UpdatesManager.h"
+#include "td/telegram/Version.h"
 #include "td/telegram/VideoNotesManager.h"
 #include "td/telegram/VideosManager.h"
 #include "td/telegram/VoiceNotesManager.h"
@@ -3001,6 +3002,8 @@ class SetBackgroundRequest : public RequestActor<> {
 Td::Td(unique_ptr<TdCallback> callback, Options options)
     : callback_(std::move(callback)), td_options_(std::move(options)) {
   CHECK(callback_ != nullptr);
+  LOG(INFO) << "Create Td with layer " << MTPROTO_LAYER << ", database version " << current_db_version()
+            << " and version " << static_cast<int32>(Version::Next) - 1;
 }
 
 Td::~Td() = default;
