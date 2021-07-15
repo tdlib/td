bb36d97482e4a70d80235a0d87d5befba0305e1f
levlam <levlam@telegram.org>
2021-07-15 03:37:14 +0300

    Don't expect leaving voice chat participants to have video.

--- td/telegram/GroupCallParticipant.cpp
+++ td/telegram/GroupCallParticipant.cpp
@@ -69,9 +69,6 @@ GroupCallParticipant::GroupCallParticipant(const tl_object_ptr<telegram_api::gro
   if (is_just_joined && get_has_video()) {
     video_diff++;
   }
-  if (joined_date == 0) {
-    video_diff--;
-  }
 }
 
 bool GroupCallParticipant::is_versioned_update(const tl_object_ptr<telegram_api::groupCallParticipant> &participant) {
