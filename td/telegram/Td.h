92c2a9c4e521df720abeaa9872e1c2b797d5c93f
levlam <levlam@telegram.org>
2022-01-31 22:07:31 +0300

    Update version to 1.8.1.

--- td/telegram/Td.h
+++ td/telegram/Td.h
@@ -98,7 +98,7 @@ class Td final : public Actor {
   Td &operator=(Td &&) = delete;
   ~Td() final;
 
-  static constexpr const char *TDLIB_VERSION = "1.8.0";
+  static constexpr const char *TDLIB_VERSION = "1.8.1";
 
   struct Options {
     std::shared_ptr<NetQueryStats> net_query_stats;
