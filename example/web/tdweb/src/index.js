242801a2f33a1076562582b49763badf01b78010
levlam <levlam@telegram.org>
2022-03-10 21:58:16 +0300

    Improve boolean fields documentation.

--- example/web/tdweb/src/index.js
+++ example/web/tdweb/src/index.js
@@ -35,7 +35,7 @@ class TdClient {
    * @param {Object} options - Options for TDLib instance creation.
    * @param {TdClient~updateCallback} options.onUpdate - Callback for all incoming updates.
    * @param {string} [options.instanceName=tdlib] - Name of the TDLib instance. Currently only one instance of TdClient with a given name is allowed. All but one instances with the same name will be automatically closed. Usually, the newest non-background instance is kept alive. Files will be stored in an IndexedDb table with the same name.
-   * @param {boolean} [options.isBackground=false] - Pass true, if the instance is opened from the background.
+   * @param {boolean} [options.isBackground=false] - Pass true if the instance is opened from the background.
    * @param {string} [options.jsLogVerbosityLevel=info] - The initial verbosity level of the JavaScript part of the code (one of 'error', 'warning', 'info', 'log', 'debug').
    * @param {number} [options.logVerbosityLevel=2] - The initial verbosity level for the TDLib internal logging (0-1023).
    * @param {boolean} [options.useDatabase=true] - Pass false to use TDLib without database and secret chats. It will significantly improve loading time, but some functionality will be unavailable.
