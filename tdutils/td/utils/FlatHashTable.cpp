254f10b88564b9337e77c09510a47575a08688f9
levlam <levlam@telegram.org>
2022-03-11 21:49:58 +0300

    Fix include.

--- tdutils/td/utils/FlatHashTable.cpp
+++ tdutils/td/utils/FlatHashTable.cpp
@@ -4,10 +4,9 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-#pragma once
+#include "td/utils/FlatHashTable.h"
 
 #include "td/utils/bits.h"
-#include "td/utils/common.h"
 #include "td/utils/Random.h"
 
 namespace td {
