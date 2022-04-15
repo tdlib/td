b17a05f3fceefa7089fa139e9e4b3ab681277223
levlam <levlam@telegram.org>
2022-04-15 16:32:12 +0300

    Increase MessageUnsupported version to reload web_app buttons.

--- td/telegram/MessageContent.cpp
+++ td/telegram/MessageContent.cpp
@@ -451,7 +451,7 @@ class MessageChatSetTtl final : public MessageContent {
 
 class MessageUnsupported final : public MessageContent {
  public:
-  static constexpr int32 CURRENT_VERSION = 10;
+  static constexpr int32 CURRENT_VERSION = 11;
   int32 version = CURRENT_VERSION;
 
   MessageUnsupported() = default;
