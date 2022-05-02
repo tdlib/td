76d687e62d6f7c5b77d9e7986a2a617588c75544
alyral <59310749+alyral@users.noreply.github.com>
2022-05-02 12:46:16 +0200

    Add session type field (#1950)

--- td/telegram/Account.cpp
+++ td/telegram/Account.cpp
@@ -28,13 +28,83 @@
 
 namespace td {
 
+static td_api::object_ptr<td_api::SessionType> get_session_type(
+    const tl_object_ptr<telegram_api::authorization> &authorization) {
+  auto contains = [](const string &str, const char *substr) {
+    return str.find(substr) != string::npos;
+  };
+  
+  const string &app_name = authorization->app_name_;
+  auto device_model = to_lower(authorization->device_model_);
+  auto platform = to_lower(authorization->platform_);
+  auto system_version = to_lower(authorization->system_version_);
+
+  if (device_model.find("xbox") != string::npos) {
+    return td_api::make_object<td_api::sessionTypeXbox>();
+  }
+
+  bool web = [&] {
+    Slice web_name("Web");
+    auto pos = app_name.find(web_name.c_str());
+    if (pos == string::npos) {
+      return false;
+    }
+
+    auto next_character = app_name[pos + web_name.size()];
+    return !('a' <= next_character && next_character <= 'z');
+  }();
+
+  if (web) {
+    if (contains(device_model, "brave")) {
+      return td_api::make_object<td_api::sessionTypeBrave>();
+    } else if (contains(device_model, "vivaldi")) {
+      return td_api::make_object<td_api::sessionTypeVivaldi>();
+    } else if (contains(device_model, "opera") || contains(device_model, "opr")) {
+      return td_api::make_object<td_api::sessionTypeOpera>();
+    } else if (contains(device_model, "edg")) {
+      return td_api::make_object<td_api::sessionTypeEdge>();
+    } else if (contains(device_model, "chrome")) {
+      return td_api::make_object<td_api::sessionTypeChrome>();
+    } else if (contains(device_model, "firefox") || contains(device_model, "fxios")) {
+      return td_api::make_object<td_api::sessionTypeFirefox>();
+    } else if (contains(device_model, "safari")) {
+      return td_api::make_object<td_api::sessionTypeSafari>();
+    }
+  }
+
+  if (begins_with(platform, "android") || contains(system_version, "android")) {
+    return td_api::make_object<td_api::sessionTypeAndroid>();
+  } else if (begins_with(platform, "windows") || contains(system_version, "windows")) {
+    return td_api::make_object<td_api::sessionTypeWindows>();
+  } else if (begins_with(platform, "ubuntu") || contains(system_version, "ubuntu")) {
+    return td_api::make_object<td_api::sessionTypeUbuntu>();
+  } else if (begins_with(platform, "linux") || contains(system_version, "linux")) {
+    return td_api::make_object<td_api::sessionTypeLinux>();
+  }
+
+  auto ios = begins_with(platform, "ios") || contains(system_version, "ios");
+  auto macos = begins_with(platform, "macos") || contains(system_version, "macos");
+  if (ios && contains(device_model, "iphone")) {
+    return td_api::make_object<td_api::sessionTypeIphone>();
+  } else if (ios && contains(device_model, "ipad")) {
+    return td_api::make_object<td_api::sessionTypeIpad>();
+  } else if (macos && contains(device_model, "mac")) {
+    return td_api::make_object<td_api::sessionTypeMac>();
+  } else if (ios || macos) {
+    return td_api::make_object<td_api::sessionTypeApple>();
+  }
+
+  return td_api::make_object<td_api::sessionTypeUnknown>();
+}
+
 static td_api::object_ptr<td_api::session> convert_authorization_object(
     tl_object_ptr<telegram_api::authorization> &&authorization) {
   CHECK(authorization != nullptr);
   return td_api::make_object<td_api::session>(
       authorization->hash_, authorization->current_, authorization->password_pending_,
-      !authorization->encrypted_requests_disabled_, !authorization->call_requests_disabled_, authorization->api_id_,
-      authorization->app_name_, authorization->app_version_, authorization->official_app_, authorization->device_model_,
+      !authorization->encrypted_requests_disabled_, !authorization->call_requests_disabled_,
+      get_session_type(authorization), authorization->api_id_, authorization->app_name_,
+      authorization->app_version_, authorization->official_app_, authorization->device_model_,
       authorization->platform_, authorization->system_version_, authorization->date_created_,
       authorization->date_active_, authorization->ip_, authorization->country_, authorization->region_);
 }
