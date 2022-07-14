bf80c43c05732905df4d27dee31536759b6f1e06
Arseny Smirnov <arseny30@gmail.com>
2022-07-14 18:36:46 +0400

    fix CE

--- td/telegram/net/NetQueryDispatcher.cpp
+++ td/telegram/net/NetQueryDispatcher.cpp
@@ -24,6 +24,7 @@
 #include "td/utils/format.h"
 #include "td/utils/logging.h"
 #include "td/utils/misc.h"
+#include "td/utils/port/sleep.h"
 #include "td/utils/port/thread.h"
 #include "td/utils/Slice.h"
 #include "td/utils/SliceBuilder.h"
