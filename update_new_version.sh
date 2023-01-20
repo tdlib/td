71554fccdad0c8ffecd2562c7694ea580ccf7963
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2023-01-21 01:12:54 +0300

    Add update new version script.

---
+++ update_new_version.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+cd $(dirname $0)
+
+COMMIT_FILES="README.md example/cpp/CMakeLists.txt example/uwp/extension.vsixmanifest td/telegram/OptionManager.cpp"
+
+# check arguemnt '-i' to drop all fixed files list.
+for arg in "$@"; do if [[ "$arg" == "-i" ]]; then COMMIT_FILES=""; break; fi; done
+
+# 'git diff' to ensure that 'CMakeLists.txt' is modifided.
+GIT_DIFF=$(git --no-pager diff --unified=0 CMakeLists.txt)
+
+SED_REGEX="project\(TDLib VERSION ([0-9\.]+) LANGUAGES CXX C\)"
+
+OLD_TDLIB_VERSION=$(echo "${GIT_DIFF}" | sed -nr "s/\-$SED_REGEX/\1/p")
+NEW_TDLIB_VERSION=$(echo "${GIT_DIFF}" | sed -nr "s/\+$SED_REGEX/\1/p")
+
+if [ ! -z "${OLD_TDLIB_VERSION}" ] && [ ! -z "${NEW_TDLIB_VERSION}" ] &&
+   [ "${OLD_TDLIB_VERSION}" != "${NEW_TDLIB_VERSION}" ] ; then
+
+   for arg in "$@" ; do
+      if [[ "$arg" == "-i" ]] ; then
+         continue
+      elif [ -e "$arg" ] ; then
+         if [[ "$COMMIT_FILES" == "" ]] ; then
+            COMMIT_FILES="$arg"
+         else
+            COMMIT_FILES="$COMMIT_FILES $arg"
+         fi
+      else
+        echo "$arg: No such file or directory"; exit 1
+      fi
+   done
+
+   if [[ "$COMMIT_FILES" == "" ]] ; then
+     echo "No files to update."; exit 1
+   fi
+
+   # Replace all matches
+   # sed -i "s/${OLD_TDLIB_VERSION}/${NEW_TDLIB_VERSION}/g" $COMMIT_FILES || exit 1
+
+   # Replace first match
+   sed -i "0,/${OLD_TDLIB_VERSION}/s//${NEW_TDLIB_VERSION}/g" $COMMIT_FILES || exit 1
+
+   git --no-pager diff CMakeLists.txt $COMMIT_FILES
+
+   echo -e "\n\n"
+   read -p "Commit \"Update version to ${NEW_TDLIB_VERSION}.\" (y/n)? " answer
+
+   if [[ "${answer}" == "Y" ]] || [[ "${answer}" == "y" ]] ; then
+      git commit --quiet CMakeLists.txt $COMMIT_FILES -m "Update version to ${NEW_TDLIB_VERSION}."; echo
+      git --no-pager log --stat -n 1
+   else
+      # Undo sed changes
+      sed -i "0,/${NEW_TDLIB_VERSION}/s//${OLD_TDLIB_VERSION}/g" $COMMIT_FILES
+      echo "Aborted."; exit 1
+   fi
+else
+   echo "Couldn't find new TDLib version."
+fi
