1f3de5bd990c0b01bc48cf5727c3ca8d57d33c02
levlam <levlam@telegram.org>
2023-02-13 15:43:55 +0300

    Update src.sh/src.ps1.

--- src.ps1
+++ src.ps1
@@ -1,1 +1,1 @@
-﻿git ls-tree -r HEAD --name-only benchmark example memprof td tdactor tddb tdnet tdtl tdutils test tg_http_client | Select-String "\.cpp$|\.h$|\.hpp$"
+﻿git ls-tree -r HEAD --name-only benchmark example memprof td tdactor tddb tdnet tdtl tdutils test | Select-String "\.cpp$|\.h$|\.hpp$"
--- src.sh
+++ src.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git ls-tree -r HEAD --name-only benchmark example memprof td tdactor tddb tdnet tdtl tdutils test tg_http_client | grep -E "\.cpp$|\.h$|\.hpp$"
+git ls-tree -r HEAD --name-only benchmark example memprof td tdactor tddb tdnet tdtl tdutils test | grep -E "\.cpp$|\.h$|\.hpp$"
