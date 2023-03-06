36701d3acd3aab56844de07882fe7e283c6ada3d
levlam <levlam@telegram.org>
2023-03-06 15:46:22 +0300

    Use 2-space indentation in update_version.sh.

--- update_version.sh
+++ update_version.sh
@@ -15,42 +15,42 @@ OLD_TDLIB_VERSION=$(echo "${GIT_DIFF}" | sed -nr "s/\-$SED_REGEX/\1/p")
 NEW_TDLIB_VERSION=$(echo "${GIT_DIFF}" | sed -nr "s/\+$SED_REGEX/\1/p")
 
 if [ ! -z "${OLD_TDLIB_VERSION}" ] && [ ! -z "${NEW_TDLIB_VERSION}" ] &&
-   [ "${OLD_TDLIB_VERSION}" != "${NEW_TDLIB_VERSION}" ] ; then
+  [ "${OLD_TDLIB_VERSION}" != "${NEW_TDLIB_VERSION}" ] ; then
 
-   for arg in "$@" ; do
-      if [[ "$arg" == "-i" ]] ; then
-         continue
-      elif [ -e "$arg" ] ; then
-         if [[ "$COMMIT_FILES" == "" ]] ; then
-            COMMIT_FILES="$arg"
-         else
-            COMMIT_FILES="$COMMIT_FILES $arg"
-         fi
+  for arg in "$@" ; do
+    if [[ "$arg" == "-i" ]] ; then
+      continue
+    elif [ -e "$arg" ] ; then
+      if [[ "$COMMIT_FILES" == "" ]] ; then
+        COMMIT_FILES="$arg"
       else
-        echo "$arg: No such file or directory"; exit 1
+        COMMIT_FILES="$COMMIT_FILES $arg"
       fi
-   done
+    else
+      echo "$arg: No such file or directory"; exit 1
+    fi
+  done
 
-   if [[ "$COMMIT_FILES" == "" ]] ; then
-     echo "No files to update."; exit 1
-   fi
+  if [[ "$COMMIT_FILES" == "" ]] ; then
+    echo "No files to update."; exit 1
+  fi
 
-   # Replace all matches
-   sed --binary -i "s/${OLD_TDLIB_VERSION//./\\.}/${NEW_TDLIB_VERSION}/g" $COMMIT_FILES || exit 1
+  # Replace all matches
+  sed --binary -i "s/${OLD_TDLIB_VERSION//./\\.}/${NEW_TDLIB_VERSION}/g" $COMMIT_FILES || exit 1
 
-   git --no-pager diff CMakeLists.txt $COMMIT_FILES
+  git --no-pager diff CMakeLists.txt $COMMIT_FILES
 
-   echo -e "\n\n"
-   read -p "Commit \"Update version to ${NEW_TDLIB_VERSION}.\" (y/n)? " answer
+  echo -e "\n\n"
+  read -p "Commit \"Update version to ${NEW_TDLIB_VERSION}.\" (y/n)? " answer
 
-   if [[ "${answer}" == "Y" ]] || [[ "${answer}" == "y" ]] ; then
-      git commit --quiet -n CMakeLists.txt $COMMIT_FILES -m "Update version to ${NEW_TDLIB_VERSION}."; echo
-      git --no-pager log --stat -n 1
-   else
-      # Undo sed changes
-      sed --binary -i "s/${NEW_TDLIB_VERSION//./\\.}/${OLD_TDLIB_VERSION}/g" $COMMIT_FILES || exit 1
-      echo "Aborted."; exit 1
-   fi
+  if [[ "${answer}" == "Y" ]] || [[ "${answer}" == "y" ]] ; then
+    git commit --quiet -n CMakeLists.txt $COMMIT_FILES -m "Update version to ${NEW_TDLIB_VERSION}."; echo
+    git --no-pager log --stat -n 1
+  else
+    # Undo sed changes
+    sed --binary -i "s/${NEW_TDLIB_VERSION//./\\.}/${OLD_TDLIB_VERSION}/g" $COMMIT_FILES || exit 1
+    echo "Aborted."; exit 1
+  fi
 else
-   echo "Couldn't find new TDLib version."
+  echo "Couldn't find new TDLib version."
 fi
