46b2d79ca9d9fa52857ece60db1d0597fb3fa73d
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2023-06-30 15:10:39 +0800

    update example to build openssl 3.1.1

--- example/ios/build-openssl.sh
+++ example/ios/build-openssl.sh
@@ -3,12 +3,11 @@ cd $(dirname $0)
 
 git clone https://github.com/beeware/Python-Apple-support
 cd Python-Apple-support
-git checkout 60b990128d5f1f04c336ff66594574515ab56604 || exit 1
+git checkout 6f43aba0ddd5a9f52f39775d0141bd4363614020 || exit 1
 git reset --hard || exit 1
 git apply ../Python-Apple-support.patch || exit 1
 cd ..
 
-#TODO: change openssl version
 platforms="macOS iOS watchOS tvOS"
 
 for platform in $platforms;
@@ -31,8 +30,8 @@ do
     cd ..
     rm -rf third_party/openssl/$platform || exit 1
     mkdir -p third_party/openssl/$platform/lib || exit 1
-    cp ./Python-Apple-support/build/$platform/libcrypto.a third_party/openssl/$platform/lib/ || exit 1
-    cp ./Python-Apple-support/build/$platform/libssl.a third_party/openssl/$platform/lib/ || exit 1
-    cp -r ./Python-Apple-support/build/$platform/openssl/include/ third_party/openssl/$platform/include || exit 1
+    cp ./Python-Apple-support/merge/$platform/openssl/lib/libcrypto.a third_party/openssl/$platform/lib/ || exit 1
+    cp ./Python-Apple-support/merge/$platform/openssl/lib/libssl.a third_party/openssl/$platform/lib/ || exit 1
+    cp -r ./Python-Apple-support/merge/$platform/openssl/include/ third_party/openssl/$platform/include || exit 1
   done
 done
