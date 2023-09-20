cc1b01620bc4b8b5c98aed59a8ada641b0c788e3
levlam <levlam@telegram.org>
2023-09-20 19:09:51 +0300

    Drop cached channel lists when dialog database is recreated.

--- td/telegram/TdDb.cpp
+++ td/telegram/TdDb.cpp
@@ -425,6 +425,8 @@ Status TdDb::init_sqlite(const Parameters &parameters, const DbKey &key, const D
     binlog_pmc.erase("dlds_counter");
     binlog_pmc.erase_by_prefix("dlds#");
     binlog_pmc.erase("fetched_marks_as_unread");
+    binlog_pmc.erase_by_prefix("public_channels");
+    binlog_pmc.erase("channels_to_send_stories");
   }
   if (user_version == 0) {
     binlog_pmc.erase("next_contacts_sync_date");
