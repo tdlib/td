26d63f605bdc52c0de168cffd27535c08d234f79
levlam <levlam@telegram.org>
2024-01-17 12:22:56 +0300

    Rename chat permission to can_create_topics.

--- td/telegram/DialogParticipant.cpp
+++ td/telegram/DialogParticipant.cpp
@@ -246,7 +246,7 @@ RestrictedRights::RestrictedRights(const td_api::object_ptr<td_api::chatPermissi
                            rights->can_send_other_messages_, rights->can_send_other_messages_,
                            rights->can_send_other_messages_, rights->can_add_web_page_previews_,
                            rights->can_send_polls_, rights->can_change_info_, rights->can_invite_users_,
-                           rights->can_pin_messages_, rights->can_manage_topics_, channel_type);
+                           rights->can_pin_messages_, rights->can_create_topics_, channel_type);
 }
 
 RestrictedRights::RestrictedRights(bool can_send_messages, bool can_send_audios, bool can_send_documents,
