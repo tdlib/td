7144475524b13b9fae2759e4d5ba38bde97bc1fa
levlam <levlam@telegram.org>
2024-01-26 23:07:39 +0300

    Increase MessageUnsupported version.

--- td/telegram/MessageContent.cpp
+++ td/telegram/MessageContent.cpp
@@ -498,7 +498,7 @@ class MessageChatSetTtl final : public MessageContent {
 
 class MessageUnsupported final : public MessageContent {
  public:
-  static constexpr int32 CURRENT_VERSION = 27;
+  static constexpr int32 CURRENT_VERSION = 28;
   int32 version = CURRENT_VERSION;
 
   MessageUnsupported() = default;
