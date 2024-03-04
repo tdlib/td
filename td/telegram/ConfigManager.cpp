e61c4d03e59d63748f2cbdacd1ae7600ae716e11
levlam <levlam@telegram.org>
2024-03-04 13:28:55 +0300

    Add "quick_reply_message_count_max" option.

--- td/telegram/ConfigManager.cpp
+++ td/telegram/ConfigManager.cpp
@@ -1985,6 +1985,10 @@ void ConfigManager::process_app_config(tl_object_ptr<telegram_api::JSONValue> &c
                                 get_json_value_int(std::move(key_value->value_), key));
         continue;
       }
+      if (key == "quick_reply_messages_limit") {
+        G()->set_option_integer("quick_reply_message_count_max", get_json_value_int(std::move(key_value->value_), key));
+        continue;
+      }
 
       new_values.push_back(std::move(key_value));
     }
