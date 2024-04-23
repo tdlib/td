e812370dc50a84c1ff42df725b109bf4fa9f7718
levlam <levlam@telegram.org>
2024-04-23 12:07:48 +0300

    Add class td_api::chatRevenueAmount.

--- td/telegram/StatisticsManager.cpp
+++ td/telegram/StatisticsManager.cpp
@@ -274,13 +274,19 @@ static int64 get_amount(int64 amount, bool allow_negative = false) {
   return amount;
 }
 
+static td_api::object_ptr<td_api::chatRevenueAmount> convert_broadcast_revenue_balances(
+    telegram_api::object_ptr<telegram_api::broadcastRevenueBalances> obj) {
+  CHECK(obj != nullptr);
+  return td_api::make_object<td_api::chatRevenueAmount>(
+      "TON", get_amount(obj->overall_revenue_), get_amount(obj->current_balance_), get_amount(obj->available_balance_));
+}
+
 static td_api::object_ptr<td_api::chatRevenueStatistics> convert_broadcast_revenue_stats(
     telegram_api::object_ptr<telegram_api::stats_broadcastRevenueStats> obj) {
   CHECK(obj != nullptr);
   return td_api::make_object<td_api::chatRevenueStatistics>(
-      convert_stats_graph(std::move(obj->top_hours_graph_)), convert_stats_graph(std::move(obj->revenue_graph_)), "TON",
-      get_amount(obj->balances_->overall_revenue_), get_amount(obj->balances_->current_balance_),
-      get_amount(obj->balances_->available_balance_),
+      convert_stats_graph(std::move(obj->top_hours_graph_)), convert_stats_graph(std::move(obj->revenue_graph_)),
+      convert_broadcast_revenue_balances(std::move(obj->balances_)),
       obj->usd_rate_ > 0 ? clamp(obj->usd_rate_ * 1e-7, 1e-18, 1e18) : 1.0);
 }
 
