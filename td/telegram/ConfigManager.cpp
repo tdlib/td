84167ef1574228f6c1a12f00548db6081c1237d4
levlam <levlam@telegram.org>
2024-05-22 14:26:55 +0300

    Add "fact_check_length_max" option.

--- td/telegram/ConfigManager.cpp
+++ td/telegram/ConfigManager.cpp
@@ -2001,6 +2001,11 @@ void ConfigManager::process_app_config(tl_object_ptr<telegram_api::JSONValue> &c
         can_edit_fact_check = get_json_value_bool(std::move(key_value->value_), key);
         continue;
       }
+      if (key == "factcheck_length_limit") {
+        G()->set_option_integer("fact_check_length_max",
+                                get_json_value_int(std::move(key_value->value_), key));
+        continue;
+      }
 
       new_values.push_back(std::move(key_value));
     }
