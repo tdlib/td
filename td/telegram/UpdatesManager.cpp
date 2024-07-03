2820a9d281154fc9fc81fbbae976d20057b8f6b1
levlam <levlam@telegram.org>
2024-07-03 11:58:51 +0300

    Return back missing call to MessagesManager::before_get_difference.

--- td/telegram/UpdatesManager.cpp
+++ td/telegram/UpdatesManager.cpp
@@ -528,6 +528,8 @@ void UpdatesManager::before_get_difference(bool is_initial) {
   // may be called many times before after_get_difference is called
   send_closure(G()->state_manager(), &StateManager::on_synchronized, false);
 
+  td_->messages_manager_->before_get_difference();
+
   vector<Promise<Unit>> promises;
   if (can_postpone_updates()) {
     for (auto &update : pending_pts_updates_) {
