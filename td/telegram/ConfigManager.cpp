1e3ea181d4e1cac6971f8edad5828300b93bcc77
levlam <levlam@telegram.org>
2024-07-23 12:27:07 +0300

    Add "can_preload_weather" option.

--- td/telegram/ConfigManager.cpp
+++ td/telegram/ConfigManager.cpp
@@ -2042,6 +2042,10 @@ void ConfigManager::process_app_config(tl_object_ptr<telegram_api::JSONValue> &c
         G()->set_option_integer("bot_media_preview_count_max", get_json_value_int(std::move(key_value->value_), key));
         continue;
       }
+      if (key == "story_weather_preload") {
+        G()->set_option_integer("can_preload_weather", get_json_value_bool(std::move(key_value->value_), key));
+        continue;
+      }
 
       new_values.push_back(std::move(key_value));
     }
