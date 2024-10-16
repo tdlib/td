1d5acce32d6fadbd0d4f032872b729b64be18b1f
levlam <levlam@telegram.org>
2024-10-16 13:43:49 +0300

    tg_cli: add td_api::inputStoryAreaTypeWeather support.

--- td/telegram/cli.cpp
+++ td/telegram/cli.cpp
@@ -1410,6 +1410,8 @@ class CliClient final : public Actor {
                                                                         as_message_id(message_id));
         } else if (area[0] == 'u') {
           type = td_api::make_object<td_api::inputStoryAreaTypeLink>(area.substr(1));
+        } else if (area[0] == 'w') {
+          type = td_api::make_object<td_api::inputStoryAreaTypeWeather>(20.1, "☀️", to_integer<int32>(area.substr(1)));
         }
         result->areas_.push_back(td_api::make_object<td_api::inputStoryArea>(std::move(position), std::move(type)));
       }
