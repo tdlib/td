a0895ec6dd1d6658b6bf2938ae29edad95466879
levlam <levlam@telegram.org>
2024-10-28 16:59:14 +0300

    Fix log message in Status::clone_static.

--- tdutils/td/utils/Status.h
+++ tdutils/td/utils/Status.h
@@ -368,7 +368,7 @@ class Status {
   }
 
   Status clone_static(int code) const TD_WARN_UNUSED_RESULT {
-    LOG_CHECK(ptr_ != nullptr && get_info().static_flag) << ptr_.get() << ' ' << code;
+    LOG_CHECK(ptr_ != nullptr && get_info().static_flag) << static_cast<const void *>(ptr_.get()) << ' ' << code;
     Status result;
     result.ptr_ = std::unique_ptr<char[], Deleter>(ptr_.get());
     return result;
