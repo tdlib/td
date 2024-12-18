2be9e799a2bc523550d4f83f4d2d66d41c9573b9
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2024-12-18 13:18:16 +0300

    Fix warning.

--- td/telegram/files/ResourceState.cpp
+++ td/telegram/files/ResourceState.cpp
@@ -4,8 +4,6 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-#pragma once
-
 #include "td/telegram/files/ResourceState.h"
 
 #include "td/utils/format.h"
