1fe2b3897e0c097172d525f53aaedf0e3bf9b8bf
levlam <levlam@telegram.org>
2025-02-24 18:22:05 +0300

    Add "pinned_gift_count_max" option.

--- td/telegram/ConfigManager.cpp
+++ td/telegram/ConfigManager.cpp
@@ -2070,6 +2070,10 @@ void ConfigManager::process_app_config(tl_object_ptr<telegram_api::JSONValue> &c
                                 get_json_value_int(std::move(key_value->value_), key));
         continue;
       }
+      if (key == "stargifts_pinned_to_top_limit") {
+        G()->set_option_integer("pinned_gift_count_max", get_json_value_int(std::move(key_value->value_), key));
+        continue;
+      }
 
       new_values.push_back(std::move(key_value));
     }
--- td/telegram/OptionManager.cpp
+++ td/telegram/OptionManager.cpp
@@ -172,6 +172,7 @@ OptionManager::OptionManager(Td *td)
   set_default_integer_option("bot_verification_custom_description_length_max", 70);
   set_default_integer_option("paid_message_star_count_max", 10000);
   set_default_integer_option("paid_message_earnings_per_mille", 850);
+  set_default_integer_option("pinned_gift_count_max", 6);
 
   if (options.isset("my_phone_number") || !options.isset("my_id")) {
     update_premium_options();
