caccd02a3959b07f0af6a7c05b93216ef7c6a632
levlam <levlam@telegram.org>
2025-04-08 18:49:00 +0300

    Remove groupCall.from_call_id.

--- td/telegram/GroupCallManager.cpp
+++ td/telegram/GroupCallManager.cpp
@@ -906,7 +906,6 @@ struct GroupCallManager::GroupCall {
   int32 unmuted_video_limit = 0;
   DcId stream_dc_id;
   DialogId as_dialog_id;
-  CallId call_id;
 
   int32 version = -1;
   int32 leave_version = -1;
@@ -4839,12 +4838,11 @@ tl_object_ptr<td_api::groupCall> GroupCallManager::get_group_call_object(
   int32 record_duration = record_start_date == 0 ? 0 : max(G()->unix_time() - record_start_date + 1, 1);
   bool is_video_recorded = get_group_call_is_video_recorded(group_call);
   return td_api::make_object<td_api::groupCall>(
-      group_call->group_call_id.get(), group_call->call_id.get(), get_group_call_title(group_call),
-      scheduled_start_date, start_subscribed, is_active, group_call->is_rtmp_stream, is_joined, group_call->need_rejoin,
-      group_call->can_be_managed, group_call->participant_count, group_call->has_hidden_listeners,
-      group_call->loaded_all_participants, std::move(recent_speakers), is_my_video_enabled, is_my_video_paused,
-      can_enable_video, mute_new_participants, can_toggle_mute_new_participants, record_duration, is_video_recorded,
-      group_call->duration);
+      group_call->group_call_id.get(), get_group_call_title(group_call), scheduled_start_date, start_subscribed,
+      is_active, group_call->is_rtmp_stream, is_joined, group_call->need_rejoin, group_call->can_be_managed,
+      group_call->participant_count, group_call->has_hidden_listeners, group_call->loaded_all_participants,
+      std::move(recent_speakers), is_my_video_enabled, is_my_video_paused, can_enable_video, mute_new_participants,
+      can_toggle_mute_new_participants, record_duration, is_video_recorded, group_call->duration);
 }
 
 tl_object_ptr<td_api::updateGroupCall> GroupCallManager::get_update_group_call_object(
