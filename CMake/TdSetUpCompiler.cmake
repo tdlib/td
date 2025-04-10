0a96edcf317dbf6880ce3bc8a5c4ac7b98239dd0
levlam <levlam@telegram.org>
2025-04-10 14:00:37 +0300

    Disable stringop-overflow warning in GCC regardless of compiler version, because it produces false positives in the old versions too.

--- CMake/TdSetUpCompiler.cmake
+++ CMake/TdSetUpCompiler.cmake
@@ -154,7 +154,7 @@ function(td_set_up_compiler)
     # see http://www.open-std.org/jtc1/sc22/wg21/docs/cwg_defects.html#1579
     add_cxx_compiler_flag("-Wno-redundant-move")
   endif()
-  if (GCC AND NOT (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.0))
+  if (GCC)
     add_cxx_compiler_flag("-Wno-stringop-overflow")  # some false positives
   endif()
   if (CLANG AND (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.5))
