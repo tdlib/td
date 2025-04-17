8e980df73e7f8677ba60466e244d389609e6ea7c
levlam <levlam@telegram.org>
2025-04-17 16:45:08 +0300

    Update documentation of searchCallMessages.

--- tde2e/td/e2e/e2e_api.h
+++ tde2e/td/e2e/e2e_api.h
@@ -288,8 +288,8 @@ Result<StorageBlockchainState> storage_get_blockchain_state(StorageId);
 using CallId = std::int64_t;
 using CallChannelId = std::int32_t;
 struct CallParticipant {
-  UserId user_id;
-  PublicKeyId public_key_id;
+  UserId user_id{};
+  PublicKeyId public_key_id{};
   int permissions{};
 };
 
