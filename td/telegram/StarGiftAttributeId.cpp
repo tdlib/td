f24a7737dd3d160572b2335d90f4346d3ed752e5
levlam <levlam@telegram.org>
2025-05-05 17:21:02 +0300

    Add td_api::UpgradedGiftAttributeId.

---
+++ td/telegram/StarGiftAttributeId.cpp
@@ -0,0 +1,71 @@
+//
+// Copyright Aliaksei Levin (levlam@telegram.org), Arseny Smirnov (arseny30@gmail.com) 2014-2025
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "td/telegram/StarGiftAttributeId.h"
+
+#include "td/utils/algorithm.h"
+
+namespace td {
+
+Result<StarGiftAttributeId> StarGiftAttributeId::get_star_gift_attribute_id(
+    td_api::object_ptr<td_api::UpgradedGiftAttributeId> &&attribute) {
+  if (attribute == nullptr) {
+    return Status::Error(400, "Attribute identifier must be non-empty");
+  }
+  StarGiftAttributeId result;
+  switch (attribute->get_id()) {
+    case td_api::upgradedGiftAttributeIdModel::ID:
+      result.type_ = Type::Model;
+      result.sticker_id_ = static_cast<const td_api::upgradedGiftAttributeIdModel *>(attribute.get())->sticker_id_;
+      break;
+    case td_api::upgradedGiftAttributeIdSymbol::ID:
+      result.type_ = Type::Pattern;
+      result.sticker_id_ = static_cast<const td_api::upgradedGiftAttributeIdSymbol *>(attribute.get())->sticker_id_;
+      break;
+    case td_api::upgradedGiftAttributeIdBackdrop::ID:
+      result.type_ = Type::Model;
+      result.backdrop_id_ = static_cast<const td_api::upgradedGiftAttributeIdBackdrop *>(attribute.get())->backdrop_id_;
+      break;
+    default:
+      UNREACHABLE();
+  }
+  return result;
+}
+
+Result<vector<StarGiftAttributeId>> StarGiftAttributeId::get_star_gift_attribute_ids(
+    vector<td_api::object_ptr<td_api::UpgradedGiftAttributeId>> &&attributes) {
+  vector<StarGiftAttributeId> result;
+  for (auto &attribute : attributes) {
+    TRY_RESULT(attribute_id, get_star_gift_attribute_id(std::move(attribute)));
+    result.push_back(attribute_id);
+  }
+  return result;
+}
+
+telegram_api::object_ptr<telegram_api::StarGiftAttributeId>
+StarGiftAttributeId::get_input_star_gift_attribute_id_object() const {
+  switch (type_) {
+    case Type::Model:
+      return telegram_api::make_object<telegram_api::starGiftAttributeIdModel>(sticker_id_);
+    case Type::Pattern:
+      return telegram_api::make_object<telegram_api::starGiftAttributeIdPattern>(sticker_id_);
+    case Type::Backdrop:
+      return telegram_api::make_object<telegram_api::starGiftAttributeIdBackdrop>(backdrop_id_);
+    case Type::None:
+    default:
+      UNREACHABLE();
+      return nullptr;
+  }
+}
+
+vector<telegram_api::object_ptr<telegram_api::StarGiftAttributeId>>
+StarGiftAttributeId::get_input_star_gift_attribute_ids_object(const vector<StarGiftAttributeId> &attributes) {
+  return transform(attributes, [](const StarGiftAttributeId &attribute) {
+    return attribute.get_input_star_gift_attribute_id_object();
+  });
+}
+
+}  // namespace td
---
+++ td/telegram/StarGiftAttributeId.h
@@ -0,0 +1,37 @@
+//
+// Copyright Aliaksei Levin (levlam@telegram.org), Arseny Smirnov (arseny30@gmail.com) 2014-2025
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#pragma once
+
+#include "td/telegram/td_api.h"
+#include "td/telegram/telegram_api.h"
+
+#include "td/utils/common.h"
+#include "td/utils/Status.h"
+#include "td/utils/StringBuilder.h"
+
+namespace td {
+
+class StarGiftAttributeId {
+  enum class Type : int32 { None, Model, Pattern, Backdrop };
+  Type type_ = Type::None;
+  int64 sticker_id_ = 0;
+  int32 backdrop_id_ = 0;
+
+  static Result<StarGiftAttributeId> get_star_gift_attribute_id(
+      td_api::object_ptr<td_api::UpgradedGiftAttributeId> &&attribute);
+
+  telegram_api::object_ptr<telegram_api::StarGiftAttributeId> get_input_star_gift_attribute_id_object() const;
+
+ public:
+  static Result<vector<StarGiftAttributeId>> get_star_gift_attribute_ids(
+      vector<td_api::object_ptr<td_api::UpgradedGiftAttributeId>> &&attributes);
+
+  static vector<telegram_api::object_ptr<telegram_api::StarGiftAttributeId>> get_input_star_gift_attribute_ids_object(
+      const vector<StarGiftAttributeId> &attributes);
+};
+
+}  // namespace td
