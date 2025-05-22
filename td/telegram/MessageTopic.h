5a707bb38f9cfd3c589e28f7029a03018a8b60a8
levlam <levlam@telegram.org>
2025-05-22 20:13:38 +0300

    Add MessageTopic getters.

--- td/telegram/MessageTopic.h
+++ td/telegram/MessageTopic.h
@@ -32,10 +32,35 @@ class MessageTopic {
   MessageTopic(Td *td, DialogId dialog_id, bool is_topic_message, MessageId top_thread_message_id,
                SavedMessagesTopicId saved_messages_topic_id);
 
-  Result<MessageTopic> get_message_topic(Td *td, DialogId dialog_id,
-                                         const td_api::object_ptr<td_api::MessageTopic> &topic);
+  static Result<MessageTopic> get_message_topic(Td *td, DialogId dialog_id,
+                                                const td_api::object_ptr<td_api::MessageTopic> &topic);
 
   td_api::object_ptr<td_api::MessageTopic> get_message_topic_object(Td *td) const;
+
+  bool is_empty() const {
+    return type_ == Type::None;
+  }
+
+  MessageId get_forum_topic_id() const {
+    if (type_ != Type::Forum) {
+      return MessageId();
+    }
+    return top_thread_message_id_;
+  }
+
+  SavedMessagesTopicId get_monoforum_topic_id() const {
+    if (type_ != Type::Monoforum) {
+      return SavedMessagesTopicId();
+    }
+    return saved_messages_topic_id_;
+  }
+
+  SavedMessagesTopicId get_saved_messages_topic_id() const {
+    if (type_ != Type::SavedMessages) {
+      return SavedMessagesTopicId();
+    }
+    return saved_messages_topic_id_;
+  }
 };
 
 StringBuilder &operator<<(StringBuilder &string_builder, const MessageTopic &message_topic);
