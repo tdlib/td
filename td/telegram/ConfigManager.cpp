735cb5ec3c996d3477521e67cfc6a300cfd12d28
levlam <levlam@telegram.org>
2025-06-09 01:09:19 +0300

    Add "direct_channel_message_star_count_default" option.

--- td/telegram/ConfigManager.cpp
+++ td/telegram/ConfigManager.cpp
@@ -2075,6 +2075,11 @@ void ConfigManager::process_app_config(tl_object_ptr<telegram_api::JSONValue> &c
         G()->set_option_integer("poll_answer_count_max", get_json_value_int(std::move(key_value->value_), key));
         continue;
       }
+      if (key == "stars_paid_messages_channel_amount_default") {
+        G()->set_option_integer("direct_channel_message_star_count_default",
+                                get_json_value_int(std::move(key_value->value_), key));
+        continue;
+      }
 
       new_values.push_back(std::move(key_value));
     }
