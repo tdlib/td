63caf1cec65c80ee980f7e32b09698aa2c4bc5ab
levlam <levlam@telegram.org>
2025-06-10 15:28:07 +0300

    Add missing include.

--- td/mtproto/TlsInit.cpp
+++ td/mtproto/TlsInit.cpp
@@ -12,6 +12,7 @@
 #include "td/utils/BigNum.h"
 #include "td/utils/common.h"
 #include "td/utils/crypto.h"
+#include "td/utils/logging.h"
 #include "td/utils/Random.h"
 #include "td/utils/SliceBuilder.h"
 #include "td/utils/Span.h"
