ca46d61485204bbd1478d9bf14a5370a5cdbccc1
levlam <levlam@telegram.org>
2025-06-20 14:15:11 +0300

    Add "to_do_list_title_length_max" option.

--- td/telegram/ConfigManager.cpp
+++ td/telegram/ConfigManager.cpp
@@ -1428,6 +1428,7 @@ void ConfigManager::process_app_config(tl_object_ptr<telegram_api::JSONValue> &c
       {"story_viewers_expire_period", "story_viewers_expiration_delay"},
       {"telegram_antispam_group_size_min", "aggressive_anti_spam_supergroup_member_count_min"},
       {"todo_item_length_max", "to_do_list_task_text_length_max"},
+      {"todo_title_length_max", "to_do_list_title_length_max"},
       {"topics_pinned_limit", "pinned_forum_topic_count_max"},
       {"upload_premium_speedup_download", "premium_download_speedup"},
       {"upload_premium_speedup_notify_period", ""},
