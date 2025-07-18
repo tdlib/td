7f8b71b6f5f371c4f910cde195ec7279900c9566
levlam <levlam@telegram.org>
2025-07-18 16:45:07 +0300

    Make JsonObject.field_values_ private.

--- tdutils/td/utils/JsonBuilder.h
+++ tdutils/td/utils/JsonBuilder.h
@@ -457,11 +457,11 @@ enum class JsonValueType { Null, Number, Boolean, String, Array, Object };
 using JsonArray = vector<JsonValue>;
 
 class JsonObject {
+  vector<std::pair<Slice, JsonValue>> field_values_;
+
   const JsonValue *get_field(Slice name) const;
 
  public:
-  vector<std::pair<Slice, JsonValue>> field_values_;
-
   JsonObject() = default;
 
   explicit JsonObject(vector<std::pair<Slice, JsonValue>> &&field_values);
@@ -635,9 +635,7 @@ class JsonValue final : private Jsonable {
       }
       case Type::Object: {
         auto object = scope->enter_object();
-        for (auto &field_value : get_object().field_values_) {
-          object(field_value.first, field_value.second);
-        }
+        get_object().foreach([&](Slice name, const JsonValue &value) { object(name, value); });
         break;
       }
     }
