89ba93c0f1ca3cca1c6c4cc53afb3f9fcbf81e7b
levlam <levlam@telegram.org>
2025-08-20 14:37:50 +0300

    Simplify write_tl_to_fixed_file_count.

--- tdtl/td/tl/tl_generate.cpp
+++ tdtl/td/tl/tl_generate.cpp
@@ -904,14 +904,14 @@ bool write_tl_to_fixed_file_count(const tl_config &config, const std::string &fi
   assert(file_count > 0);
   find_complex_types(config, w);
 
-  std::map<std::string, tl_string_outputer> outs;
+  std::map<int, tl_string_outputer> outs;
 
   int file_num = 0;
   tl_string_outputer *out = nullptr;
   for (int i = 0; i < file_count; i++) {
-    outs[TL_writer::int_to_string(i)].append(w.gen_output_begin(std::string()));
+    outs[i].append(w.gen_output_begin(std::string()));
   }
-  outs["0"].append(w.gen_output_begin_once());
+  outs[0].append(w.gen_output_begin_once());
 
   std::set<std::string> request_types;
   std::set<std::string> result_types;
@@ -936,7 +936,7 @@ bool write_tl_to_fixed_file_count(const tl_config &config, const std::string &fi
 
     object_types[w.gen_main_class_name(t)] = (t->simple_constructors != 1);
     file_num = (file_num + 1) % file_count;
-    out = &outs[TL_writer::int_to_string(file_num)];
+    out = &outs[file_num];
     write_class(*out, t, request_types, result_types, w);
   }
 
@@ -950,7 +950,7 @@ bool write_tl_to_fixed_file_count(const tl_config &config, const std::string &fi
 
     function_types[w.gen_class_name(t->name)] = false;
     file_num = (file_num + 1) % file_count;
-    out = &outs[TL_writer::int_to_string(file_num)];
+    out = &outs[file_num];
     write_function(*out, t, request_types, result_types, w);
   }
 
@@ -961,15 +961,15 @@ bool write_tl_to_fixed_file_count(const tl_config &config, const std::string &fi
     }
   }
 
-  write_base_object_classes(config, outs["0"], request_types, result_types, w);
-  write_base_function_class(config, outs["0"], request_types, result_types, w);
+  write_base_object_classes(config, outs[0], request_types, result_types, w);
+  write_base_function_class(config, outs[0], request_types, result_types, w);
 
   for (int i = 0; i < file_count; i++) {
-    outs[TL_writer::int_to_string(i)].append(w.gen_output_end());
+    outs[i].append(w.gen_output_end());
   }
 
-  for (std::map<std::string, tl_string_outputer>::const_iterator it = outs.begin(); it != outs.end(); ++it) {
-    std::string file_name = file_name_prefix + "_" + it->first + file_name_suffix;
+  for (std::map<int, tl_string_outputer>::const_iterator it = outs.begin(); it != outs.end(); ++it) {
+    std::string file_name = file_name_prefix + "_" + TL_writer::int_to_string(it->first) + file_name_suffix;
     if (!put_file_contents(file_name, it->second.get_result(), w.is_documentation_generated())) {
       return false;
     }
