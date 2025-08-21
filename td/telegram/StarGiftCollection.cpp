c96580522935a070344cadf7da2fdafdb6c5deeb
levlam <levlam@telegram.org>
2025-08-21 18:03:40 +0300

    Add td_api::giftCollection.

---
+++ td/telegram/StarGiftCollection.cpp
@@ -0,0 +1,47 @@
+//
+// Copyright Aliaksei Levin (levlam@telegram.org), Arseny Smirnov (arseny30@gmail.com) 2014-2025
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "td/telegram/StarGiftCollection.h"
+
+#include "td/telegram/StickersManager.h"
+#include "td/telegram/Td.h"
+
+namespace td {
+
+StarGiftCollection::StarGiftCollection(Td *td,
+                                       telegram_api::object_ptr<telegram_api::starGiftCollection> &&gift_collection) {
+  CHECK(gift_collection != nullptr);
+  collection_id_ = StarGiftCollectionId(gift_collection->collection_id_);
+  if (!collection_id_.is_valid()) {
+    LOG(ERROR) << "Receive " << collection_id_;
+    collection_id_ = {};
+    return;
+  }
+  title_ = std::move(gift_collection->title_);
+  icon_file_id_ =
+      td->stickers_manager_
+          ->on_get_sticker_document(std::move(gift_collection->icon_), StickerFormat::Unknown, "StarGiftCollection")
+          .second;
+  gift_count_ = max(0, gift_collection->gifts_count_);
+  hash_ = gift_collection->hash_;
+}
+
+td_api::object_ptr<td_api::giftCollection> StarGiftCollection::get_gift_collection_object(Td *td) const {
+  return td_api::make_object<td_api::giftCollection>(
+      collection_id_.get(), title_, td->stickers_manager_->get_sticker_object(icon_file_id_), gift_count_);
+}
+
+bool operator==(const StarGiftCollection &lhs, const StarGiftCollection &rhs) {
+  return lhs.collection_id_ == rhs.collection_id_ && lhs.title_ == rhs.title_ &&
+         lhs.icon_file_id_ == rhs.icon_file_id_ && lhs.gift_count_ == rhs.gift_count_ && lhs.hash_ == rhs.hash_;
+}
+
+StringBuilder &operator<<(StringBuilder &string_builder, const StarGiftCollection &gift_collection) {
+  return string_builder << gift_collection.collection_id_ << ' ' << gift_collection.title_ << " with "
+                        << gift_collection.gift_count_ << " gifts";
+}
+
+}  // namespace td
---
+++ td/telegram/StarGiftCollection.h
@@ -0,0 +1,58 @@
+//
+// Copyright Aliaksei Levin (levlam@telegram.org), Arseny Smirnov (arseny30@gmail.com) 2014-2025
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#pragma once
+
+#include "td/telegram/files/FileId.h"
+#include "td/telegram/StarGiftCollectionId.h"
+#include "td/telegram/td_api.h"
+#include "td/telegram/telegram_api.h"
+
+#include "td/utils/common.h"
+#include "td/utils/StringBuilder.h"
+
+namespace td {
+
+class Td;
+
+class StarGiftCollection {
+  StarGiftCollectionId collection_id_;
+  string title_;
+  FileId icon_file_id_;
+  int32 gift_count_ = 0;
+  int64 hash_ = 0;
+
+  friend bool operator==(const StarGiftCollection &lhs, const StarGiftCollection &rhs);
+
+  friend StringBuilder &operator<<(StringBuilder &string_builder, const StarGiftCollection &gift_collection);
+
+ public:
+  StarGiftCollection() = default;
+
+  StarGiftCollection(Td *td, telegram_api::object_ptr<telegram_api::starGiftCollection> &&gift_collection);
+
+  int64 get_hash() const {
+    return hash_;
+  }
+
+  td_api::object_ptr<td_api::giftCollection> get_gift_collection_object(Td *td) const;
+
+  template <class StorerT>
+  void store(StorerT &storer) const;
+
+  template <class ParserT>
+  void parse(ParserT &parser);
+};
+
+bool operator==(const StarGiftCollection &lhs, const StarGiftCollection &rhs);
+
+inline bool operator!=(const StarGiftCollection &lhs, const StarGiftCollection &rhs) {
+  return !(lhs == rhs);
+}
+
+StringBuilder &operator<<(StringBuilder &string_builder, const StarGiftCollection &gift_collection);
+
+}  // namespace td
