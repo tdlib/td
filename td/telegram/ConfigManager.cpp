b0579f51cdcf29afcab6e77c698a8ad4b574510f
levlam <levlam@telegram.org>
2025-08-25 23:32:11 +0300

    Add "user_rating_learn_more_url" option.

--- td/telegram/ConfigManager.cpp
+++ td/telegram/ConfigManager.cpp
@@ -1927,6 +1927,10 @@ void ConfigManager::process_app_config(tl_object_ptr<telegram_api::JSONValue> &c
         G()->set_option_string("toncoin_top_up_url", get_json_value_string(std::move(key_value->value_), key));
         continue;
       }
+      if (key == "stars_rating_learnmore_url") {
+        G()->set_option_string("user_rating_learn_more_url", get_json_value_string(std::move(key_value->value_), key));
+        continue;
+      }
 
       new_values.push_back(std::move(key_value));
     }
