7318a409010030742a46092ddee43350bb6c1f6b
levlam <levlam@telegram.org>
2025-09-16 17:13:07 +0300

    Add td_api::ChatTheme.

---
+++ td/telegram/ChatTheme.cpp
@@ -0,0 +1,94 @@
+//
+// Copyright Aliaksei Levin (levlam@telegram.org), Arseny Smirnov (arseny30@gmail.com) 2014-2025
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "td/telegram/ChatTheme.h"
+
+namespace td {
+
+ChatTheme::ChatTheme(Td *td, telegram_api::object_ptr<telegram_api::ChatTheme> theme) {
+  if (theme == nullptr) {
+    return;
+  }
+  switch (theme->get_id()) {
+    case telegram_api::chatTheme::ID: {
+      auto chat_theme = telegram_api::move_object_as<telegram_api::chatTheme>(theme);
+      if (chat_theme->emoticon_.empty()) {
+        LOG(ERROR) << "Receive " << to_string(chat_theme);
+        return;
+      }
+      type_ = Type::Emoji;
+      emoji_ = std::move(chat_theme->emoticon_);
+      break;
+    }
+    case telegram_api::chatThemeUniqueGift::ID: {
+      auto chat_theme = telegram_api::move_object_as<telegram_api::chatThemeUniqueGift>(theme);
+      StarGift star_gift(td, std::move(chat_theme->gift_), true);
+      if (!star_gift.is_valid() || !star_gift.is_unique()) {
+        LOG(ERROR) << "Receive chat theme with " << star_gift;
+        return;
+      }
+
+      bool was_light = false;
+      bool was_dark = false;
+      for (auto &settings : chat_theme->theme_settings_) {
+        ThemeSettings theme_settings(td, std::move(settings));
+        if (theme_settings.is_empty()) {
+          continue;
+        }
+        if (theme_settings.are_dark()) {
+          if (!was_dark) {
+            was_dark = true;
+            dark_theme_ = std::move(theme_settings);
+          }
+        } else {
+          if (!was_light) {
+            was_light = true;
+            light_theme_ = std::move(theme_settings);
+          }
+        }
+      }
+      if (light_theme_.is_empty() || dark_theme_.is_empty()) {
+        LOG(ERROR) << "Receive chat theme with invalid themes";
+        *this = {};
+        return;
+      }
+
+      type_ = Type::Gift;
+      star_gift_ = std::move(star_gift);
+      break;
+    }
+    default:
+      UNREACHABLE();
+      break;
+  }
+}
+
+td_api::object_ptr<td_api::giftChatTheme> ChatTheme::get_gift_chat_theme_object(Td *td) const {
+  CHECK(type_ == Type::Gift);
+  return td_api::make_object<td_api::giftChatTheme>(star_gift_.get_upgraded_gift_object(td),
+                                                    light_theme_.get_theme_settings_object(td),
+                                                    dark_theme_.get_theme_settings_object(td));
+}
+
+td_api::object_ptr<td_api::ChatTheme> ChatTheme::get_chat_theme_object(Td *td) const {
+  switch (type_) {
+    case Type::Default:
+      return nullptr;
+    case Type::Emoji:
+      return td_api::make_object<td_api::chatThemeEmoji>(emoji_);
+    case Type::Gift:
+      return td_api::make_object<td_api::chatThemeGift>(get_gift_chat_theme_object(td));
+    default:
+      UNREACHABLE();
+  }
+}
+
+bool operator==(const ChatTheme &lhs, const ChatTheme &rhs) {
+  return lhs.type_ == rhs.type_ && lhs.emoji_ == rhs.emoji_ && lhs.star_gift_ == rhs.star_gift_ &&
+         lhs.light_theme_ == rhs.light_theme_ && lhs.dark_theme_ == rhs.dark_theme_;
+}
+
+}  // namespace td
