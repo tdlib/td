935e66d568bee4b98d4492037043924602169035
levlam <levlam@telegram.org>
2025-10-04 02:12:59 +0300

    Add option "group_call_message_text_length_max".

--- td/telegram/OptionManager.cpp
+++ td/telegram/OptionManager.cpp
@@ -203,6 +203,7 @@ OptionManager::OptionManager(Td *td)
   set_default_integer_option("pending_text_message_period", 30);
   set_default_integer_option("user_note_text_length_max", 128);
   set_default_integer_option("group_call_message_show_time_max", 10);
+  set_default_integer_option("group_call_message_text_length_max", 128);
 
   if (options.isset("my_phone_number") || !options.isset("my_id")) {
     update_premium_options();
