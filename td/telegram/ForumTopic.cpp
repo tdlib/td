3b64466b7e8a414febbddaa3d467ee3473f476f5
levlam <levlam@telegram.org>
2025-10-21 00:29:59 +0300

    Add updateForumTopic.draft_message.

--- td/telegram/ForumTopic.cpp
+++ td/telegram/ForumTopic.cpp
@@ -137,10 +137,11 @@ td_api::object_ptr<td_api::forumTopic> ForumTopic::get_forum_topic_object(Td *td
 
 td_api::object_ptr<td_api::updateForumTopic> ForumTopic::get_update_forum_topic_object(
     Td *td, DialogId dialog_id, ForumTopicId forum_topic_id) const {
+  auto draft_message = get_draft_message_object(td, draft_message_);
   return td_api::make_object<td_api::updateForumTopic>(
       td->dialog_manager_->get_chat_id_object(dialog_id, "updateForumTopic"), forum_topic_id.get(), is_pinned_,
       last_read_inbox_message_id_.get(), last_read_outbox_message_id_.get(), unread_mention_count_,
-      unread_reaction_count_, get_chat_notification_settings_object(&notification_settings_));
+      unread_reaction_count_, get_chat_notification_settings_object(&notification_settings_), std::move(draft_message));
 }
 
 }  // namespace td
