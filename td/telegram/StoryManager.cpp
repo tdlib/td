c6442c32a1357b83225a12951846d1790df74f1f
levlam <levlam@telegram.org>
2025-11-10 23:03:29 +0300

    Disallow to forward live stories.

--- td/telegram/StoryManager.cpp
+++ td/telegram/StoryManager.cpp
@@ -4112,7 +4112,7 @@ td_api::object_ptr<td_api::story> StoryManager::get_story_object(StoryFullId sto
   auto can_be_deleted = can_delete_story(story_full_id, story);
   auto can_be_edited = can_edit_story(story_full_id, story);
   auto can_be_forwarded = !story->noforwards_ && story_id.is_server() &&
-                          privacy_settings->get_id() == td_api::storyPrivacySettingsEveryone::ID;
+                          privacy_settings->get_id() == td_api::storyPrivacySettingsEveryone::ID && !story->is_live_;
   auto can_be_replied =
       story_id.is_server() && owner_dialog_id != changelog_dialog_id && owner_dialog_id.get_type() == DialogType::User;
   auto can_set_privacy_settings = can_set_story_privacy_settings(story_full_id, story);
