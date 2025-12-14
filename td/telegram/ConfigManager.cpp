2b8ed3fc9dbdfa13d801017be6fafe0cd77787ff
levlam <levlam@telegram.org>
2025-12-14 23:40:16 +0300

    Add the option "can_use_login_passkey".

--- td/telegram/ConfigManager.cpp
+++ td/telegram/ConfigManager.cpp
@@ -1367,6 +1367,7 @@ void ConfigManager::process_app_config(tl_object_ptr<telegram_api::JSONValue> &c
       {"channel_revenue_withdrawal_enabled", "can_withdraw_chat_revenue"},
       {"premium_gift_attach_menu_icon", "gift_premium_from_attachment_menu"},
       {"premium_gift_text_field_icon", "gift_premium_from_input_field"},
+      {"settings_display_passkeys", "can_use_login_passkey"},
       {"story_weather_preload", "can_preload_weather"},
       {"stars_gifts_enabled", "can_gift_stars"},
       {"stars_paid_messages_available", "can_enable_paid_messages"},
@@ -1473,9 +1474,9 @@ void ConfigManager::process_app_config(tl_object_ptr<telegram_api::JSONValue> &c
       {"stories_changelog_user_id", "stories_changelog_user_id"}};
   static const FlatHashSet<Slice, SliceHash> ignored_options(
       {"default_emoji_statuses_stickerset_id", "forum_upgrade_participants_min", "getfile_experimental_params",
-       "message_animated_emoji_max", "stickers_emoji_cache_time", "stories_export_nopublic_link", "test",
-       "upload_max_fileparts_default", "upload_max_fileparts_premium", "channel_color_level_min",
-       "groupcall_video_participants_max", "story_expire_period", "stories_posting",
+       "ios_display_passkeys", "message_animated_emoji_max", "stickers_emoji_cache_time",
+       "stories_export_nopublic_link", "test", "upload_max_fileparts_default", "upload_max_fileparts_premium",
+       "channel_color_level_min", "groupcall_video_participants_max", "story_expire_period", "stories_posting",
        "giveaway_gifts_purchase_available", "stars_purchase_blocked", "stargifts_blocked", "starref_program_allowed",
        "starref_connect_allowed", "stars_rating_learnmore_url", "qr_login_camera", "qr_login_code",
        "dialog_filters_enabled",
