115fb2a61b8fcaaa6ab8d765797e4849efc63329
isopen <mr_makss@mail.ru>
2025-12-23 13:16:57 +0700

    Update tdjson_example.cpp

--- example/cpp/tdjson_example.cpp
+++ example/cpp/tdjson_example.cpp
@@ -1,55 +1,172 @@
 //
-// Copyright Aliaksei Levin (levlam@telegram.org), Arseny Smirnov (arseny30@gmail.com) 2014-2025
+// Copyright testertesterov (https://t.me/testertesterov), Aliaksei Levin (levlam@telegram.org), Arseny Smirnov (arseny30@gmail.com) 2014-2025
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 #include <iostream>
+#include <string>
+#include <cstdlib>
 #include <td/telegram/td_json_client.h>
 
-// Basic example of TDLib JSON interface usage.
-// Native interface should be preferred instead in C++, so here is only an example of
-// the main event cycle, which should be essentially the same for all languages.
+class TDLibAuthHelper {
+  private:
+    int client_id;
+    bool is_authorized;
+    bool need_quit;
+    bool parameters_sent;
+
+    void send_query(const std::string& query) {
+      td_send(client_id, query.c_str());
+    }
+
+    void process_response(const std::string& response) {
+      //std::cout << "Receive: " << response << std::endl;
+
+      if (response.find("\"@type\":\"updateAuthorizationState\"") != std::string::npos) {
+        if (response.find("authorizationStateWaitTdlibParameters") != std::string::npos) {
+          if (!parameters_sent) {
+            send_tdlib_parameters();
+            parameters_sent = true;
+          }
+        } else if (response.find("authorizationStateWaitPhoneNumber") != std::string::npos) {
+          std::cout << "Please enter phone number (international format): ";
+          std::string phone_number;
+          std::getline(std::cin, phone_number);
+
+          std::string query = R"({
+              "@type": "setAuthenticationPhoneNumber",
+              "phone_number": ")" + phone_number + R"("
+          })";
+          send_query(query);
+        } else if (response.find("authorizationStateWaitCode") != std::string::npos) {
+          std::cout << "Please enter code: ";
+          std::string code;
+          std::getline(std::cin, code);
+
+          std::string query = R"({
+              "@type": "checkAuthenticationCode",
+              "code": ")" + code + R"("
+          })";
+          send_query(query);
+        } else if (response.find("authorizationStateWaitPassword") != std::string::npos) {
+          std::cout << "Please enter password: ";
+          std::string password;
+          std::getline(std::cin, password);
+
+          std::string query = R"({
+              "@type": "checkAuthenticationPassword",
+              "password": ")" + password + R"("
+          })";
+          send_query(query);
+        } else if (response.find("authorizationStateReady") != std::string::npos) {
+          std::cout << "Authorization complete! You are now logged in." << std::endl;
+          is_authorized = true;
+          need_quit = true;
+        } else if (response.find("authorizationStateClosed") != std::string::npos) {
+          std::cout << "Authorization state closed." << std::endl;
+          need_quit = true;
+        } else if (response.find("authorizationStateWaitPremiumPurchase") != std::string::npos) {
+          std::cout << "Telegram Premium subscription is required." << std::endl;
+          need_quit = true;
+        }
+      } else if (response.find("\"@type\":\"error\"") != std::string::npos) {
+        std::cout << "ERROR: " << response << std::endl;
+        if (response.find("Valid api_id must be provided") != std::string::npos) {
+          std::cout << "\n=== API CREDENTIALS ERROR ===" << std::endl;
+          std::cout << "Please check your api_id and api_hash." << std::endl;
+          std::cout << "Get them from: https://my.telegram.org" << std::endl;
+          need_quit = true;
+        }
+      }
+    }
+
+    void send_tdlib_parameters() {
+      std::string query = R"({
+        "@type": "setTdlibParameters",
+        "database_directory": "tdlib_data",
+        "use_message_database": true,
+        "use_secret_chats": true,
+        "api_id": )" + get_api_id() + R"(,
+        "api_hash": ")" + get_api_hash() + R"(",
+        "system_language_code": "en",
+        "device_model": "C++ TDLib Client",
+        "application_version": "1.1"
+      })";
+
+      std::cout << "Setting TDLib parameters..." << std::endl;
+      send_query(query);
+    }
+
+    std::string get_api_id() {
+      const char* api_id_env = std::getenv("TD_API_ID");
+      if (api_id_env) {
+        return api_id_env;
+      }
+      return "94575";
+    }
+
+    std::string get_api_hash() {
+      const char* api_hash_env = std::getenv("TD_API_HASH");
+      if (api_hash_env) {
+        return api_hash_env;
+      }
+      return "a3406de8d171bb422bb6ddf3bbd800e2";
+    }
+
+  public:
+    TDLibAuthHelper(int id) : client_id(id), is_authorized(false), 
+                              need_quit(false), parameters_sent(false) {}
+
+    void run_auth_flow() {
+      const double WAIT_TIMEOUT = 1.0;
+
+      std::cout << "Starting Telegram authentication flow..." << std::endl;
+      std::cout << "Press Ctrl+C to cancel at any time." << std::endl;
+
+      send_query("{\"@type\":\"getOption\",\"name\":\"version\"}");
+
+      while (!need_quit) {
+        const char *result = td_receive(WAIT_TIMEOUT);
+        if (result != nullptr && result[0] != '\0') {
+          process_response(result);
+        }
+      }
+    }
+
+    bool is_auth_complete() const {
+      return is_authorized;
+    }
+};
 
 int main() {
-  // disable TDLib logging
-  td_execute("{\"@type\":\"setLogVerbosityLevel\", \"new_verbosity_level\":0}");
+  td_execute("{\"@type\":\"setLogVerbosityLevel\",\"new_verbosity_level\":1}");
 
   int client_id = td_create_client_id();
-  // somehow share the client_id with other threads, which will be able to send requests via td_send
 
-  // start the client by sending request to it
-  td_send(client_id, "{\"@type\":\"getOption\", \"name\":\"version\"}");
-
-  const bool test_incorrect_queries = false;
-  if (test_incorrect_queries) {
-    td_execute("{\"@type\":\"setLogVerbosityLevel\", \"new_verbosity_level\":1}");
-    td_execute("");
-    td_execute("test");
-    td_execute("\"test\"");
-    td_execute("{\"@type\":\"test\", \"@extra\":1}");
-
-    td_send(client_id, "{\"@type\":\"getFileMimeType\"}");
-    td_send(client_id, "{\"@type\":\"getFileMimeType\", \"@extra\":1}");
-    td_send(client_id, "{\"@type\":\"getFileMimeType\", \"@extra\":null}");
-    td_send(client_id, "{\"@type\":\"test\"}");
-    td_send(client_id, "[]");
-    td_send(client_id, "{\"@type\":\"test\", \"@extra\":1}");
-    td_send(client_id, "{\"@type\":\"sendMessage\", \"chat_id\":true, \"@extra\":1}");
-    td_send(client_id, "test");
+  std::cout << "Testing TDLib execute method..." << std::endl;
+  const char* test_result = td_execute(
+    "{\"@type\":\"getTextEntities\","
+    "\"text\":\"@telegram /test_command https://telegram.org telegram.me\"}"
+  );
+  if (test_result) {
+    std::cout << "Text entities: " << test_result << std::endl;
   }
 
-  const double WAIT_TIMEOUT = 10.0;  // seconds
-  while (true) {
-    const char *result = td_receive(WAIT_TIMEOUT);
-    if (result != nullptr) {
-      // parse the result as a JSON object and process it as an incoming update or an answer to a previously sent request
+  TDLibAuthHelper auth_helper(client_id);
 
-      // if (result is UpdateAuthorizationState with authorizationStateClosed) {
-      //   break;
-      // }
+  auth_helper.run_auth_flow();
 
-      std::cout << result << std::endl;
+  if (auth_helper.is_auth_complete()) {
+    const double WAIT_TIMEOUT = 1.0;
+    while (true) {
+      const char *result = td_receive(WAIT_TIMEOUT);
+      if (result != nullptr && result[0] != '\0') {
+        std::cout << result << std::endl;
+      }
     }
   }
+
+  std::cout << "Exiting..." << std::endl;
+  return 0;
 }
