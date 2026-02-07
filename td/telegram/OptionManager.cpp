11e254af695060d8890024dd7faa1cc2d6685ef8
levlam <levlam@telegram.org>
2026-02-07 03:12:10 +0300

    Update version to 1.8.61.

--- td/telegram/OptionManager.cpp
+++ td/telegram/OptionManager.cpp
@@ -772,7 +772,7 @@ td_api::object_ptr<td_api::OptionValue> OptionManager::get_option_synchronously(
       break;
     case 'v':
       if (name == "version") {
-        return td_api::make_object<td_api::optionValueString>("1.8.60");
+        return td_api::make_object<td_api::optionValueString>("1.8.61");
       }
       break;
   }
