tdlib/td

commit c7f013608884f090d46eb0a51c9aae429d2c45b0

BrowserProfile, Builder, TL-parser refactor

td/mtproto/ClientHelloExecutor.cpp
@@ -1,0 +1,354 @@
+// SPDX-FileCopyrightText: Copyright 2026 telemt community
+// SPDX-License-Identifier: MIT
+// telemt: https://github.com/telemt
+// telemt: https://t.me/telemtrs
+//
+
+#include "td/mtproto/ClientHelloExecutor.h"
+
+#include "td/mtproto/ProxySecret.h"
+
+#include "td/utils/BigNum.h"
+#include "td/utils/common.h"
+#include "td/utils/crypto.h"
+#include "td/utils/Random.h"
+
+#include <openssl/bn.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+
+#include <cstring>
+
+namespace td {
+namespace mtproto {
+
+namespace {
+
+using Op = ClientHelloOp;
+
+void init_grease_values(MutableSlice res) {
+  Random::secure_bytes(res);
+  for (auto &c : res) {
+    c = static_cast<char>((c & 0xF0) + 0x0A);
+  }
+  for (size_t i = 1; i < res.size(); i += 2) {
+    if (res[i] == res[i - 1]) {
+      res[i] ^= 0x10;
+    }
+  }
+}
+
+class ExecutionContext {
+ public:
+  ExecutionContext(const ExecutorConfig &config, Slice domain)
+      : grease_values_(config.grease_value_count, '\0')
+      , domain_(domain.str())
+      , config_(config) {
+    init_grease_values(MutableSlice(grease_values_));
+  }
+
+  char get_grease(size_t index) const {
+    CHECK(index < grease_values_.size());
+    return grease_values_[index];
+  }
+
+  size_t grease_size() const {
+    return grease_values_.size();
+  }
+
+  Slice domain() const {
+    return Slice(domain_).substr(0, ProxySecret::MAX_DOMAIN_LENGTH);
+  }
+
+  const ExecutorConfig &config() const {
+    return config_;
+  }
+
+ private:
+  string grease_values_;
+  string domain_;
+  const ExecutorConfig &config_;
+};
+
+class LengthCalculator {
+ public:
+  void append(const Op &op, const ExecutionContext &context) {
+    switch (op.type) {
+      case Op::Type::Bytes:
+        size_ += op.data.size();
+        break;
+      case Op::Type::RandomBytes:
+      case Op::Type::ZeroBytes:
+        size_ += op.length;
+        break;
+      case Op::Type::Domain:
+        size_ += context.domain().size();
+        break;
+      case Op::Type::GreaseValue:
+        size_ += 2;
+        break;
+      case Op::Type::X25519KeyShareEntry:
+        size_ += 2 + 2 + 32;
+        break;
+      case Op::Type::Secp256r1KeyShareEntry:
+        size_ += 2 + 2 + 65;
+        break;
+      case Op::Type::X25519MlKem768KeyShareEntry:
+        size_ += 2 + 2 + 1184;
+        break;
+      case Op::Type::Scope16Begin:
+        size_ += 2;
+        scope_offsets_.push_back(size_);
+        break;
+      case Op::Type::Scope16End:
+        CHECK(!scope_offsets_.empty());
+        scope_offsets_.pop_back();
+        break;
+      case Op::Type::Permutation: {
+        auto parts = op.permutation_parts;
+        Random::shuffle(parts);
+        for (const auto &part : parts) {
+          for (const auto &sub_op : part) {
+            append(sub_op, context);
+          }
+        }
+        break;
+      }
+      case Op::Type::PaddingToTarget:
+        if (size_ < static_cast<size_t>(op.value)) {
+          size_ = static_cast<size_t>(op.value + 4);
+        }
+        break;
+      default:
+        UNREACHABLE();
+    }
+  }
+
+  Result<size_t> finish() {
+    if (!scope_offsets_.empty()) {
+      return Status::Error("Unbalanced scopes");
+    }
+    return size_;
+  }
+
+ private:
+  size_t size_{0};
+  vector<size_t> scope_offsets_;
+};
+
+class ByteWriter {
+ public:
+  explicit ByteWriter(MutableSlice dest) : all_(dest), remaining_(dest) {
+  }
+
+  void append(const Op &op, const ExecutionContext &context) {
+    switch (op.type) {
+      case Op::Type::Bytes:
+        remaining_.copy_from(op.data);
+        remaining_.remove_prefix(op.data.size());
+        break;
+      case Op::Type::RandomBytes:
+        Random::secure_bytes(remaining_.substr(0, op.length));
+        remaining_.remove_prefix(op.length);
+        break;
+      case Op::Type::ZeroBytes:
+        std::memset(remaining_.begin(), 0, op.length);
+        remaining_.remove_prefix(op.length);
+        break;
+      case Op::Type::Domain: {
+        auto domain = context.domain();
+        remaining_.copy_from(domain);
+        remaining_.remove_prefix(domain.size());
+        break;
+      }
+      case Op::Type::GreaseValue: {
+        auto grease = context.get_grease(op.value);
+        remaining_[0] = grease;
+        remaining_[1] = grease;
+        remaining_.remove_prefix(2);
+        break;
+      }
+      case Op::Type::X25519KeyShareEntry:
+        append(Op::bytes("\x00\x1d\x00\x20"), context);
+        store_x25519_key_share();
+        break;
+      case Op::Type::Secp256r1KeyShareEntry:
+        append(Op::bytes("\x00\x17\x00\x41"), context);
+        store_secp256r1_key_share();
+        break;
+      case Op::Type::X25519MlKem768KeyShareEntry:
+        append(Op::bytes("\x11\xec\x04\xa0"), context);
+        store_mlkem_key_share();
+        break;
+      case Op::Type::Scope16Begin:
+        scope_offsets_.push_back(offset());
+        remaining_.remove_prefix(2);
+        break;
+      case Op::Type::Scope16End: {
+        auto begin = scope_offsets_.back();
+        scope_offsets_.pop_back();
+        auto size = offset() - begin - 2;
+        all_[begin] = static_cast<char>((size >> 8) & 0xff);
+        all_[begin + 1] = static_cast<char>(size & 0xff);
+        break;
+      }
+      case Op::Type::Permutation: {
+        auto parts = op.permutation_parts;
+        Random::shuffle(parts);
+        for (const auto &part : parts) {
+          for (const auto &sub_op : part) {
+            append(sub_op, context);
+          }
+        }
+        break;
+      }
+      case Op::Type::PaddingToTarget: {
+        auto need = op.value - static_cast<int>(offset());
+        if (need > 0) {
+          append(Op::bytes("\x00\x15"), context);
+          append(Op::scope16_begin(), context);
+          append(Op::zero_bytes(need), context);
+          append(Op::scope16_end(), context);
+        }
+        break;
+      }
+      default:
+        UNREACHABLE();
+    }
+  }
+
+  void finalize(Slice secret, int32 unix_time) {
+    auto hash_dest = all_.substr(11, 32);
+    hmac_sha256(secret, all_, hash_dest);
+    int32 old = static_cast<int32>(static_cast<uint8>(hash_dest[28])) |
+                (static_cast<int32>(static_cast<uint8>(hash_dest[29])) << 8) |
+                (static_cast<int32>(static_cast<uint8>(hash_dest[30])) << 16) |
+                (static_cast<int32>(static_cast<uint8>(hash_dest[31])) << 24);
+    int32 masked = old ^ unix_time;
+    hash_dest[28] = static_cast<char>(masked & 0xff);
+    hash_dest[29] = static_cast<char>((masked >> 8) & 0xff);
+    hash_dest[30] = static_cast<char>((masked >> 16) & 0xff);
+    hash_dest[31] = static_cast<char>((masked >> 24) & 0xff);
+  }
+
+ private:
+  static BigNum get_y2(BigNum &x, const BigNum &mod, BigNumContext &ctx) {
+    BigNum y = x.clone();
+    BigNum coef = BigNum::from_decimal("486662").move_as_ok();
+    BigNum::mod_add(y, y, coef, mod, ctx);
+    BigNum::mod_mul(y, y, x, mod, ctx);
+    BigNum one = BigNum::from_decimal("1").move_as_ok();
+    BigNum::mod_add(y, y, one, mod, ctx);
+    BigNum::mod_mul(y, y, x, mod, ctx);
+    return y;
+  }
+
+  static BigNum get_double_x(BigNum &x, const BigNum &mod, BigNumContext &ctx) {
+    BigNum denominator = get_y2(x, mod, ctx);
+    BigNum coef = BigNum::from_decimal("4").move_as_ok();
+    BigNum::mod_mul(denominator, denominator, coef, mod, ctx);
+
+    BigNum numerator;
+    BigNum::mod_mul(numerator, x, x, mod, ctx);
+    BigNum one = BigNum::from_decimal("1").move_as_ok();
+    BigNum::mod_sub(numerator, numerator, one, mod, ctx);
+    BigNum::mod_mul(numerator, numerator, numerator, mod, ctx);
+
+    auto inverse = BigNum::mod_inverse(denominator, mod, ctx);
+    if (inverse.is_ok()) {
+      denominator = inverse.move_as_ok();
+    }
+    BigNum::mod_mul(numerator, numerator, denominator, mod, ctx);
+    return numerator;
+  }
+
+  static bool is_quadratic_residue(const BigNum &a) {
+    BigNum mod = BigNum::from_hex("7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffed").move_as_ok();
+    BigNum pow = BigNum::from_hex("3ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff6").move_as_ok();
+    BigNumContext ctx;
+    BigNum r;
+    BigNum::mod_exp(r, a, pow, mod, ctx);
+    return r.to_decimal() == "1";
+  }
+
+  void store_x25519_key_share() {
+    BigNum mod = BigNum::from_hex("7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffed").move_as_ok();
+    BigNumContext ctx;
+    auto key = remaining_.substr(0, 32);
+    while (true) {
+      Random::secure_bytes(key);
+      key[31] = static_cast<char>(key[31] & 127);
+      BigNum x = BigNum::from_binary(key);
+      BigNum y = get_y2(x, mod, ctx);
+      if (is_quadratic_residue(y)) {
+        for (int i = 0; i < 3; i++) {
+          x = get_double_x(x, mod, ctx);
+        }
+        key.copy_from(x.to_le_binary(32));
+        break;
+      }
+    }
+    remaining_.remove_prefix(32);
+  }
+
+  void store_mlkem_key_share() {
+    for (size_t i = 0; i < 384; i++) {
+      auto a = Random::secure_uint32() % 3329;
+      auto b = Random::secure_uint32() % 3329;
+      remaining_[0] = static_cast<char>(a & 255);
+      remaining_[1] = static_cast<char>((a >> 8) + ((b & 15) << 4));
+      remaining_[2] = static_cast<char>(b >> 4);
+      remaining_.remove_prefix(3);
+    }
+    Random::secure_bytes(remaining_.substr(0, 32));
+    remaining_.remove_prefix(32);
+  }
+
+  void store_secp256r1_key_share() {
+    EC_KEY *ec_key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
+    CHECK(ec_key != nullptr);
+    CHECK(EC_KEY_generate_key(ec_key) == 1);
+    const EC_GROUP *group = EC_KEY_get0_group(ec_key);
+    const EC_POINT *public_key = EC_KEY_get0_public_key(ec_key);
+    CHECK(group != nullptr);
+    CHECK(public_key != nullptr);
+    auto dest = remaining_.substr(0, 65);
+    size_t written = EC_POINT_point2oct(group, public_key, POINT_CONVERSION_UNCOMPRESSED,
+                                        reinterpret_cast<unsigned char *>(dest.begin()), 65, nullptr);
+    CHECK(written == 65);
+    EC_KEY_free(ec_key);
+    remaining_.remove_prefix(65);
+  }
+
+  size_t offset() const {
+    return all_.size() - remaining_.size();
+  }
+
+  MutableSlice all_;
+  MutableSlice remaining_;
+  vector<size_t> scope_offsets_;
+};
+
+}  // namespace
+
+Result<string> ClientHelloExecutor::execute(const vector<ClientHelloOp> &ops, Slice domain, Slice secret, int32 unix_time,
+                                            const ExecutorConfig &config) {
+  CHECK(secret.size() == 16);
+  CHECK(!domain.empty());
+  ExecutionContext context(config, domain);
+  LengthCalculator calc;
+  for (auto &op : ops) {
+    calc.append(op, context);
+  }
+  TRY_RESULT(length, calc.finish());
+  string result(length, '\0');
+  ByteWriter writer(result);
+  for (auto &op : ops) {
+    writer.append(op, context);
+  }
+  writer.finalize(secret, unix_time);
+  return result;
+}
+
+}  // namespace mtproto
+}  // namespace td

td/mtproto/ClientHelloExecutor.h
@@ -1,0 +1,31 @@
+// SPDX-FileCopyrightText: Copyright 2026 telemt community
+// SPDX-License-Identifier: MIT
+// telemt: https://github.com/telemt
+// telemt: https://t.me/telemtrs
+//
+
+#pragma once
+
+#include "td/mtproto/ClientHelloOp.h"
+
+#include "td/utils/Status.h"
+
+namespace td {
+namespace mtproto {
+
+struct ExecutorConfig {
+  size_t grease_value_count{7};
+  bool has_ech{false};
+  int ech_payload_length{144};
+  int ech_enc_key_length{32};
+  uint16 alps_type{0};
+};
+
+class ClientHelloExecutor {
+ public:
+  static Result<string> execute(const vector<ClientHelloOp> &ops, Slice domain, Slice secret, int32 unix_time,
+                                const ExecutorConfig &config);
+};
+
+}  // namespace mtproto
+}  // namespace td

CMakeLists.txt
@@ -6,7 +6,17 @@
 
 cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
 
+if(CMAKE_GENERATOR MATCHES "NMake Makefiles|Ninja")
+  unset(CMAKE_GENERATOR_PLATFORM CACHE)
+  unset(CMAKE_GENERATOR_PLATFORM)
+endif()
+
 project(TDLib VERSION 1.8.63 LANGUAGES CXX C)
+
+if(MSVC AND CMAKE_GENERATOR MATCHES "NMake")
+  string(APPEND CMAKE_EXE_LINKER_FLAGS " /INCREMENTAL:NO")
+  string(APPEND CMAKE_SHARED_LINKER_FLAGS " /INCREMENTAL:NO")
+endif()
 
 if (NOT DEFINED CMAKE_MODULE_PATH)
   set(CMAKE_MODULE_PATH "")
@@ -297,6 +307,9 @@ set(TL_DOTNET_SCHEME_SOURCE
 
 set(TD_MTPROTO_SOURCE
   td/mtproto/AuthData.cpp
+  td/mtproto/BrowserProfile.cpp
+  td/mtproto/ClientHelloExecutor.cpp
+  td/mtproto/ClientHelloOpMapper.cpp
   td/mtproto/ConnectionManager.cpp
   td/mtproto/DhHandshake.cpp
   td/mtproto/Handshake.cpp
@@ -329,6 +342,10 @@ set(TD_MTPROTO_SOURCE
 
   td/mtproto/AuthData.h
   td/mtproto/AuthKey.h
+  td/mtproto/BrowserProfile.h
+  td/mtproto/ClientHelloExecutor.h
+  td/mtproto/ClientHelloOp.h
+  td/mtproto/ClientHelloOpMapper.h
   td/mtproto/ConnectionManager.h
   td/mtproto/CryptoStorer.h
   td/mtproto/DhCallback.h
@@ -1348,7 +1365,7 @@ endif()
 add_library(tdmtproto STATIC ${TD_MTPROTO_SOURCE})
 target_include_directories(tdmtproto PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}> $<BUILD_INTERFACE:${TL_TD_AUTO_INCLUDE_DIR}>)
 target_include_directories(tdmtproto SYSTEM PRIVATE ${OPENSSL_INCLUDE_DIR})
-target_link_libraries(tdmtproto PUBLIC tdactor tdnet tdutils PRIVATE ${OPENSSL_CRYPTO_LIBRARY} ${CMAKE_DL_LIBS} ${ZLIB_LIBRARIES})
+target_link_libraries(tdmtproto PUBLIC tdutils tdnet tdactor PRIVATE ${OPENSSL_CRYPTO_LIBRARY} ${CMAKE_DL_LIBS} ${ZLIB_LIBRARIES})
 if (WIN32)
   if (MINGW)
     target_link_libraries(tdmtproto PRIVATE ws2_32 mswsock crypt32)
@@ -1364,11 +1381,11 @@ endif()
 if (MSVC AND TD_ENABLE_LTO)
   add_library(tdcore_part1 STATIC ${TDLIB_SOURCE_PART1})
   target_include_directories(tdcore_part1 PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}> $<BUILD_INTERFACE:${TL_TD_AUTO_INCLUDE_DIR}>)
-  target_link_libraries(tdcore_part1 PUBLIC tdapi tdnet tddb tdactor tde2e tdutils PRIVATE tdmtproto)
+  target_link_libraries(tdcore_part1 PUBLIC tdutils tdnet tddb tde2e tdapi tdactor PRIVATE tdmtproto)
 
   add_library(tdcore_part2 STATIC ${TDLIB_SOURCE_PART2})
   target_include_directories(tdcore_part2 PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}> $<BUILD_INTERFACE:${TL_TD_AUTO_INCLUDE_DIR}>)
-  target_link_libraries(tdcore_part2 PUBLIC tdapi tdnet tddb tdactor tde2e tdutils PRIVATE tdmtproto)
+  target_link_libraries(tdcore_part2 PUBLIC tdutils tdnet tddb tde2e tdapi tdactor PRIVATE tdmtproto)
 
   add_library(tdcore INTERFACE)
   target_link_libraries(tdcore INTERFACE tdcore_part1 tdcore_part2)
@@ -1377,7 +1394,7 @@ if (MSVC AND TD_ENABLE_LTO)
 else()
   add_library(tdcore STATIC ${TDLIB_SOURCE})
   target_include_directories(tdcore PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}> $<BUILD_INTERFACE:${TL_TD_AUTO_INCLUDE_DIR}>)
-  target_link_libraries(tdcore PUBLIC tdapi tdnet tddb tdactor tde2e tdutils PRIVATE tdmtproto)
+  target_link_libraries(tdcore PUBLIC tdutils tdnet tddb tde2e tdapi tdactor PRIVATE tdmtproto)
 
   set(TD_CORE_PART_TARGETS)
 endif()

commit b39bad5593e5f390471113b9cfe0474fe3e663dc

Plumb IRng through ClientHelloExecutor + restore length variability
Three more regressions from the c7f013608 refactor caught and fixed by
the rebuilt MSVC-native test runner.

REG-9 — ClientHello length collapsed to single fingerprint per ECH mode
================================================================

`TlsRoutePolicy::*ClientHelloLengthMustNotCollapseToSingleFingerprint`
asserted that across 256 builds with different unix timestamps the wire
size takes more than one distinct value. The c7f013608 refactor lost
the per-build padding entropy randomization that the old builder used,
so for ECH-disabled routes (unknown / RU) the wire collapsed to a fixed
length and became a hashable fingerprint.

Fix:
  * Add `padding_target_entropy` to `ExecutorConfig`. Sampled per build
    in `make_config` / `build_default_hello_impl` from
    `rng.bounded(256u)`.
  * Update `LengthCalculator` and `ByteWriter` for `Op::PaddingToTarget`
    to use `op.value + config.padding_target_entropy` as the effective
    target so the wire size varies in [op.value, op.value + 255 + 4].
  * Bump `make_chromium_desktop_layout` `padding_to_target` base from
    513 to 1600 (513 was a vestige of the legacy 517-byte Telegram
    fingerprint and was always skipped after PQ + GREASE additions
    pushed the unpadded wire over 513).
  * Gate `Op::PaddingToTarget` on `!has_ech` in both passes — real
    Chrome 144/146 captures do NOT carry a padding extension when ECH
    is enabled (verified against
    `test/analysis/fixtures/clienthello/linux_desktop/`), and emitting
    both ECH and padding produces a wire-image that no real Chrome
    client emits. The combined effect is: padding fires for ECH-off
    routes (giving length variability), and ECH carries variability
    for ECH-on routes via its own random payload length.

REG-10 — ClientHelloExecutor used the global PRNG, not the seeded RNG
================================================================

td/mtproto/BrowserProfile.cpp
@@ -264,6 +264,7 @@ BrowserProfileSpec make_chrome133_impl() {
       make_extension(TlsExtensionType::SessionTicket),
       make_raw_extension(TlsExtensionType::RenegotiationInfo, "\x00"),
       make_raw_extension(TlsExtensionType::ApplicationSettings, "\x00\x03\x02\x68\x32"),
+      make_raw_extension(TlsExtensionType::CompressCertificate, "\x02\x00\x02"),
       make_extension(TlsExtensionType::EncryptedClientHello, true),
   };
   profile.layout_template = make_chromium_desktop_layout();
@@ -294,6 +295,7 @@ BrowserProfileSpec make_chrome131_impl() {
       make_extension(TlsExtensionType::SessionTicket),
       make_raw_extension(TlsExtensionType::RenegotiationInfo, "\x00"),
       make_raw_extension(TlsExtensionType::ApplicationSettings, "\x00\x03\x02\x68\x32"),
+      make_raw_extension(TlsExtensionType::CompressCertificate, "\x02\x00\x02"),
       make_extension(TlsExtensionType::EncryptedClientHello, true),
   };
   profile.layout_template = make_chromium_desktop_layout();
@@ -324,6 +326,7 @@ BrowserProfileSpec make_chrome120_impl() {
       make_extension(TlsExtensionType::SessionTicket),
       make_raw_extension(TlsExtensionType::RenegotiationInfo, "\x00"),
       make_raw_extension(TlsExtensionType::ApplicationSettings, "\x00\x03\x02\x68\x32"),
+      make_raw_extension(TlsExtensionType::CompressCertificate, "\x02\x00\x02"),
       make_extension(TlsExtensionType::EncryptedClientHello, true),
   };
   profile.layout_template = make_chromium_desktop_layout();

td/mtproto/ClientHelloExecutor.cpp
@@ -26,8 +26,8 @@ namespace {
 
 using Op = ClientHelloOp;
 
-void init_grease_values(MutableSlice res) {
-  Random::secure_bytes(res);
+void init_grease_values(MutableSlice res, stealth::IRng &rng) {
+  rng.fill_secure_bytes(res);
   for (auto &c : res) {
     c = static_cast<char>((c & 0xF0) + 0x0A);
   }
@@ -38,13 +38,33 @@ void init_grease_values(MutableSlice res) {
   }
 }
 
+// Deterministic Fisher-Yates shuffle driven by `IRng`. Required because
+// `td::Random::shuffle` uses a global Mersenne Twister seeded from
+// /dev/urandom and is NOT reproducible from a test seed. Without this
+// helper, any wire-equality test that depends on a permuted extension
+// order would fail intermittently or always.
+template <typename T>
+void rng_shuffle(vector<T> &items, stealth::IRng &rng) {
+  if (items.size() <= 1) {
+    return;
+  }
+  for (size_t i = items.size() - 1; i > 0; i--) {
+    auto j = static_cast<size_t>(rng.bounded(static_cast<uint32>(i + 1)));
+    if (j != i) {
+      using std::swap;
+      swap(items[i], items[j]);
+    }
+  }
+}
+
 class ExecutionContext {
  public:
-  ExecutionContext(const ExecutorConfig &config, Slice domain)
+  ExecutionContext(const ExecutorConfig &config, Slice domain, stealth::IRng &rng)
       : grease_values_(config.grease_value_count, '\0')
       , domain_(domain.str())
-      , config_(config) {
-    init_grease_values(MutableSlice(grease_values_));
+      , config_(config)
+      , rng_(rng) {
+    init_grease_values(MutableSlice(grease_values_), rng_);
   }
 
   char get_grease(size_t index) const {
@@ -64,10 +84,15 @@ class ExecutionContext {
     return config_;
   }
 
+  stealth::IRng &rng() const {
+    return rng_;
+  }
+
  private:
   string grease_values_;
   string domain_;
   const ExecutorConfig &config_;
+  stealth::IRng &rng_;
 };
 
 class LengthCalculator {
@@ -118,7 +143,7 @@ class LengthCalculator {
         break;
       case Op::Type::Permutation: {
         auto parts = op.permutation_parts;
-        Random::shuffle(parts);
+        rng_shuffle(parts, context.rng());
         for (const auto &part : parts) {
           for (const auto &sub_op : part) {
             append(sub_op, context);
@@ -127,6 +152,14 @@ class LengthCalculator {
         break;
       }
       case Op::Type::PaddingToTarget: {
+        // Padding only fires when ECH is disabled — ECH payload length
+        // already provides per-build wire-size variability (144..240),
+        // and emitting both ECH AND padding produces a wire-image that
+        // no real Chrome client emits (verified against
+        // chrome144/chrome146 captures, neither has 0x0015).
+        if (context.config().has_ech) {
+          break;
+        }
         auto effective_target = static_cast<size_t>(op.value) + static_cast<size_t>(context.config().padding_target_entropy);
         if (size_ < effective_target) {
           size_ = effective_target + 4;
@@ -162,7 +195,7 @@ class ByteWriter {
         remaining_.remove_prefix(op.data.size());
         break;
       case Op::Type::RandomBytes:
-        Random::secure_bytes(remaining_.substr(0, op.length));
+        context.rng().fill_secure_bytes(remaining_.substr(0, op.length));
         remaining_.remove_prefix(op.length);
         break;
       case Op::Type::ZeroBytes:
@@ -184,7 +217,7 @@ class ByteWriter {
       }
       case Op::Type::X25519KeyShareEntry:
         append(Op::bytes("\x00\x1d\x00\x20"), context);
-        store_x25519_key_share();
+        store_x25519_key_share(context.rng());
         break;
       case Op::Type::Secp256r1KeyShareEntry:
         append(Op::bytes("\x00\x17\x00\x41"), context);
@@ -197,8 +230,8 @@ class ByteWriter {
         // `test/stealth/test_pq_hybrid_key_share_format_invariants.cpp`
         // for the regression guard.
         append(Op::bytes("\x11\xec\x04\xc0"), context);
-        store_mlkem_key_share();
-        store_x25519_key_share();
+        store_mlkem_key_share(context.rng());
+        store_x25519_key_share(context.rng());
         break;
       case Op::Type::GreaseKeyShareEntry: {
         // GREASE key_share entry: 2 bytes group (paired GREASE byte) +
@@ -216,7 +249,7 @@ class ByteWriter {
         break;
       }
       case Op::Type::X25519PublicKey:
-        store_x25519_key_share();
+        store_x25519_key_share(context.rng());
         break;
       case Op::Type::Scope16Begin:
         scope_offsets_.push_back(offset());
@@ -232,7 +265,7 @@ class ByteWriter {
       }
       case Op::Type::Permutation: {
         auto parts = op.permutation_parts;
-        Random::shuffle(parts);
+        rng_shuffle(parts, context.rng());
         for (const auto &part : parts) {
           for (const auto &sub_op : part) {
             append(sub_op, context);
@@ -241,6 +274,11 @@ class ByteWriter {
         break;
       }
       case Op::Type::PaddingToTarget: {
+        // Padding only fires when ECH is disabled — see the matching
+        // gate in `LengthCalculator::PaddingToTarget` for the rationale.
+        if (context.config().has_ech) {
+          break;
+        }
         auto effective_target = op.value + context.config().padding_target_entropy;
         auto need = effective_target - static_cast<int>(offset());
         if (need > 0) {
@@ -310,12 +348,12 @@ class ByteWriter {
     return r.to_decimal() == "1";
   }
 
-  void store_x25519_key_share() {
+  void store_x25519_key_share(stealth::IRng &rng) {
     BigNum mod = BigNum::from_hex("7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffed").move_as_ok();
     BigNumContext ctx;
     auto key = remaining_.substr(0, 32);
     while (true) {
-      Random::secure_bytes(key);
+      rng.fill_secure_bytes(key);
       key[31] = static_cast<char>(key[31] & 127);
       BigNum x = BigNum::from_binary(key);
       BigNum y = get_y2(x, mod, ctx);
@@ -330,16 +368,16 @@ class ByteWriter {
     remaining_.remove_prefix(32);
   }
 
-  void store_mlkem_key_share() {
+  void store_mlkem_key_share(stealth::IRng &rng) {
     for (size_t i = 0; i < 384; i++) {
-      auto a = Random::secure_uint32() % 3329;
-      auto b = Random::secure_uint32() % 3329;
+      auto a = rng.secure_uint32() % 3329;
+      auto b = rng.secure_uint32() % 3329;
       remaining_[0] = static_cast<char>(a & 255);
       remaining_[1] = static_cast<char>((a >> 8) + ((b & 15) << 4));
       remaining_[2] = static_cast<char>(b >> 4);
       remaining_.remove_prefix(3);
     }
-    Random::secure_bytes(remaining_.substr(0, 32));
+    rng.fill_secure_bytes(remaining_.substr(0, 32));
     remaining_.remove_prefix(32);
   }
 
@@ -371,10 +409,10 @@ class ByteWriter {
 }  // namespace
 
 Result<string> ClientHelloExecutor::execute(const vector<ClientHelloOp> &ops, Slice domain, Slice secret, int32 unix_time,
-                                            const ExecutorConfig &config) {
+                                            const ExecutorConfig &config, stealth::IRng &rng) {
   CHECK(secret.size() == 16);
   CHECK(!domain.empty());
-  ExecutionContext context(config, domain);
+  ExecutionContext context(config, domain, rng);
   LengthCalculator calc;
   for (auto &op : ops) {
     calc.append(op, context);

td/mtproto/ClientHelloExecutor.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "td/mtproto/ClientHelloOp.h"
+#include "td/mtproto/stealth/Interfaces.h"
 
 #include "td/utils/Status.h"
 
@@ -29,8 +30,17 @@ struct ExecutorConfig {
 
 class ClientHelloExecutor {
  public:
+  // The `rng` parameter MUST drive every byte of randomness consumed
+  // during ClientHello generation: GREASE pool initialization, ECH and
+  // padding random bodies, X25519 / ML-KEM rejection sampling, and the
+  // `Permutation` shuffle. Production callers pass a `SecureRng` that
+  // wraps the global crypto PRNG; tests pass a deterministic `MockRng`
+  // so that wire-image equality assertions are reproducible across
+  // builds. Without an injected `IRng`, the executor would silently
+  // fall back to the global PRNG and the determinism of any seeded
+  // test would collapse.
   static Result<string> execute(const vector<ClientHelloOp> &ops, Slice domain, Slice secret, int32 unix_time,
-                                const ExecutorConfig &config);
+                                const ExecutorConfig &config, stealth::IRng &rng);
 };
 
 }  // namespace mtproto

td/mtproto/stealth/TlsHelloBuilder.cpp
@@ -72,7 +72,7 @@ string build_tls_hello_impl(string domain, Slice secret, int32 unix_time, Browse
   auto config = make_config(spec, enable_ech, rng);
   auto &profile = mtproto::get_profile_spec(profile_id);
   auto ops = mtproto::ClientHelloOpMapper::map(profile, config);
-  auto result = mtproto::ClientHelloExecutor::execute(ops, domain, secret, unix_time, config);
+  auto result = mtproto::ClientHelloExecutor::execute(ops, domain, secret, unix_time, config, rng);
   CHECK(result.is_ok());
   return result.move_as_ok();
 }
@@ -93,7 +93,7 @@ string build_default_hello_impl(string domain, Slice secret, int32 unix_time,
   config.padding_target_entropy = static_cast<int>(rng.bounded(256u));
 
   auto ops = mtproto::ClientHelloOpMapper::map(profile, config);
-  auto result = mtproto::ClientHelloExecutor::execute(ops, domain, secret, unix_time, config);
+  auto result = mtproto::ClientHelloExecutor::execute(ops, domain, secret, unix_time, config, rng);
   CHECK(result.is_ok());
   return result.move_as_ok();
 }
@@ -118,7 +118,7 @@ string build_default_tls_client_hello_with_options(string domain, Slice secret, 
   config.alps_type = options.alps_extension_type;
 
   auto ops = mtproto::ClientHelloOpMapper::map(profile, config);
-  auto result = mtproto::ClientHelloExecutor::execute(ops, domain, secret, unix_time, config);
+  auto result = mtproto::ClientHelloExecutor::execute(ops, domain, secret, unix_time, config, rng);
   CHECK(result.is_ok());
   return result.move_as_ok();
 }