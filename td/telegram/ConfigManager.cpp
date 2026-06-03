a5a345f20e1b644f11368485e6c1ffc09a575d73
levlam <levlam@telegram.org>
2026-06-03 22:53:28 +0300

    Add option "rich_message_depth_max".

--- td/telegram/ConfigManager.cpp
+++ td/telegram/ConfigManager.cpp
@@ -1429,6 +1429,7 @@ void ConfigManager::process_app_config(tl_object_ptr<telegram_api::JSONValue> &c
       {"reactions_user_max_premium", ""},
       {"rich_message_length_limit", "rich_message_text_length_max"},
       {"rich_message_max_blocks", "rich_message_block_count_max"},
+      {"rich_message_max_depth", "rich_message_depth_max"},
       {"ringtone_duration_max", "notification_sound_duration_max"},
       {"ringtone_saved_count_max", "notification_sound_count_max"},
       {"ringtone_size_max", "notification_sound_size_max"},
--- td/telegram/OptionManager.cpp
+++ td/telegram/OptionManager.cpp
@@ -218,6 +218,7 @@ OptionManager::OptionManager(Td *td)
   set_default_integer_option("message_text_length_max", 4096);
   set_default_integer_option("rich_message_text_length_max", 32768);
   set_default_integer_option("rich_message_block_count_max", 500);
+  set_default_integer_option("rich_message_depth_max", 16);
 
   if (options.isset("my_phone_number") || !options.isset("my_id")) {
     update_premium_options();
